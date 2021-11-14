23e7b3dba3fc5763a8b7071d98c630bd36d4be84
jp calvo <calvojp92@gmail.com>
2021-11-15 06:55:26 +0800

    Added schema/models for user routes

--- api/api/models.py
+++ api/api/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
-from typing import Optional
+from typing import Generic, Optional, TypeVar
 
+from pydantic.generics import GenericModel
 from pydantic.networks import EmailStr
 from sqlmodel import Column, DateTime, Field, SQLModel, String
 
@@ -39,6 +40,38 @@ class User(Timestamp, table=True):
     @email_verified.setter
     def email_verified(self, v: bool):
         self.email_verified_at = datetime.now(timezone.utc) if v else None
+
+
+class ReadUser(SQLModel):
+    id: int
+    name: str
+    email: EmailStr
+    email_verified: bool
+    email_verified_at: Optional[datetime] = None
+    created_at: datetime
+    updated_at: Optional[datetime] = None
+
+
+class CreateUser(SQLModel):
+    name: str = Field(..., min_length=2, max_length=25)
+    email: EmailStr
+    password: str = Field(..., min_length=5, max_length=50)
+
+
+class UpdateUser(SQLModel):
+    name: Optional[str] = Field(default=None, min_length=2, max_length=25)
+    email: Optional[EmailStr] = None
+
+
+PaginatedT = TypeVar('PaginatedT')
+
+
+class Paginated(GenericModel, Generic[PaginatedT]):
+    page: int
+    page_size: int
+    rows: list[PaginatedT]
+    total_rows: int
+    has_next: bool
 
 
 def generate_models():
