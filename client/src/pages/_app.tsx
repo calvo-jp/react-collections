9c8f5ac2017916c58019d1960b4e6029f5ebd93a
jp calvo <calvojp92@gmail.com>
2021-11-17 08:56:37 +0800

    Setup font-family

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -10,7 +10,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
-      <div className="min-h-screen bg-white text-gray-700">
+      <div className="min-h-screen bg-white text-gray-700 font-sans">
         <Component {...pageProps} />
       </div>
     </React.Fragment>
