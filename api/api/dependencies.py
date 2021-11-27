81a4b62ddb896ca90c20098d9878ff5ca1cbf3cd
jp calvo <calvojp92@gmail.com>
2021-11-27 13:33:28 +0800

    added more deps

--- api/api/dependencies.py
+++ api/api/dependencies.py
@@ -1,6 +1,8 @@
+from typing import Callable, Optional
+
 from fastapi import Request
 from fastapi.exceptions import HTTPException
-from fastapi.param_functions import Depends
+from fastapi.param_functions import Depends, Path
 from fastapi.security.oauth2 import OAuth2PasswordBearer
 from jose import ExpiredSignatureError, JWTError
 from sqlalchemy.exc import NoResultFound
@@ -32,3 +34,52 @@ async def get_current_user(request: Request, user_id: int = Depends(get_current_
             return user
         except NoResultFound as error:
             raise HTTPException(401, 'Invalid token') from error
+
+
+def verify_owner(path_userid_alias: Optional[str] = None):
+    """
+    A decorator that raises a `403` error if user id in path
+    does not match the user id found in token.
+    Be default the param name is `id`, but you can also pass a custom name or alias.
+    Note that this should not be used like a regular dependecy.
+    See example usage below.
+
+    ```
+    @verify_owner()
+    @router.post(path='/{id}', ...)
+
+    @verify_owner('user_id')
+    @router.patch(path='/{user_id}', ...)
+    ```
+    """
+
+    if path_userid_alias is None:
+        path_userid_alias = 'id'
+
+    def verify_owner_(
+        path_userid: int = Path(..., alias=path_userid_alias),
+        token_userid: int = Depends(get_current_user_id)
+    ):
+        if token_userid != path_userid:
+            raise HTTPException(403, 'Error making changes to other account')
+
+    def wrapper(callback: Callable):
+        def inner(*args, **kwargs):
+            dependencies: list = kwargs.get('dependencies') or []
+            dependencies.append(Depends(verify_owner_))
+            kwargs['dependencies'] = dependencies
+            return callback(*args, **kwargs, dependencies=dependencies)
+        return inner
+    return wrapper
+
+
+class UserSearchQuery:
+    def __init__(
+        self,
+        page: Optional[int] = None,
+        page_size: Optional[int] = None,
+        search: Optional[str] = None
+    ):
+        self.page = page or 1
+        self.page_size = page_size or 25
+        self.search = search
