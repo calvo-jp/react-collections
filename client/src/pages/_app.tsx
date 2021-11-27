4c4d877f45c32fbe1deb39264bbaee297b0c5fa9
jp calvo <calvojp92@gmail.com>
2021-11-27 19:47:20 +0800

    used cdn for font

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,3 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/700.css';
 import GlobalProvider from 'hooks/store/GlobalProvider';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
