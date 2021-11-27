e21061132f57132439e4e6c3a03c14f0f79e4ca4
jp calvo <calvojp92@gmail.com>
2021-11-27 10:40:52 +0800

    added and refactored dependency

--- api/api/dependencies.py
+++ api/api/dependencies.py
@@ -3,35 +3,32 @@ from fastapi.exceptions import HTTPException
 from fastapi.param_functions import Depends
 from fastapi.security.oauth2 import OAuth2PasswordBearer
 from jose import ExpiredSignatureError, JWTError
+from sqlalchemy.exc import NoResultFound
 from sqlmodel import Session, select
 
 from .config import engine
 from .models import User
 from .utils import jsonwebtoken
 
-_oauth2_scheme = OAuth2PasswordBearer('/sessions')
+oauth2_scheme = OAuth2PasswordBearer('/sessions')
 
 
-async def get_current_user(request: Request, token: str = Depends(_oauth2_scheme)):
-    """Gets user from token and adds it to app current state"""
-
+async def get_current_user_id(request: Request, token: str = Depends(oauth2_scheme)) -> int:
     try:
         claims = jsonwebtoken.decode(token)
-        user_id = claims.get('sub')
-
-        if user_id is None:
-            raise JWTError()
-
-        with Session(engine) as session:
-            stmt = select(User).where(User.id == user_id)
-            user = session.exec(stmt).one_or_none()
-
-            if not user:
-                raise JWTError()
-
-            request.state.current_user = user
-            return user
+        request.state.current_user_id = claims['sub']
+        return request.state.current_user_id
     except ExpiredSignatureError as error:
         raise HTTPException(401, 'Expired token') from error
     except JWTError as error:
         raise HTTPException(401, 'Invalid token') from error
+
+
+async def get_current_user(request: Request, user_id: int = Depends(get_current_user_id)):
+    with Session(engine) as session:
+        try:
+            user = session.exec(select(User).where(User.id == user_id)).one()
+            request.state.current_user = user
+            return user
+        except NoResultFound as error:
+            raise HTTPException(404, 'Invalid token') from error
