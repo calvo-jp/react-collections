b866169f05252c45c7c34d4326dfd2634c92b3fe
jp calvo <calvojp92@gmail.com>
2021-11-27 22:28:02 +0800

    Initial socket route for validator

--- api/api/main.py
+++ api/api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .models import generate_models
-from .routers import place, session, user
+from .routers import place, session, user, validator
 
 app = FastAPI()
 
@@ -16,6 +16,7 @@ app.add_middleware(
 app.include_router(session.router)
 app.include_router(user.router)
 app.include_router(place.router)
+app.include_router(validator.router)
 
 
 @app.on_event('startup')
