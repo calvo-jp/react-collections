1ce2f5eac886d83ccc2597bf30e52262b049e3f3
jp calvo <calvojp92@gmail.com>
2021-12-02 20:40:49 +0800

    added dependency to get user from token

---
+++ api/api/dependencies.py
@@ -0,0 +1,37 @@
+from fastapi import status
+from fastapi.exceptions import HTTPException
+from fastapi.param_functions import Depends
+from fastapi.requests import Request
+from fastapi.security.oauth2 import OAuth2PasswordBearer
+from jose.exceptions import ExpiredSignatureError, JWTError
+from sqlalchemy.exc import NoResultFound
+from sqlmodel import Session, select
+
+from .config import engine
+from .models import User
+from .utils import jsonwebtoken
+
+_oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/sessions')
+
+
+async def get_current_user(request: Request, token: str = Depends(_oauth2_scheme)):
+    try:
+        claims = jsonwebtoken.decode(token)
+
+        with Session(engine) as session:
+            stmt = select(User).where(User.id == claims['sub'])
+            user = session.exec(stmt).one()
+
+            request.state.current_user = user
+
+            return user
+    except ExpiredSignatureError as error:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Expired token.'
+        ) from error
+    except (JWTError, NoResultFound) as error:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid token.'
+        ) from error
