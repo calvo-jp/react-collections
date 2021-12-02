5ff0fbb48957ab53daf3682113f573cc0070d8f2
jp calvo <calvojp92@gmail.com>
2021-12-02 19:13:36 +0800

    added config

---
+++ api/api/config.py
@@ -0,0 +1,49 @@
+# pylint: disable=consider-using-f-string
+
+from enum import Enum
+from typing import Optional
+
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+from sqlmodel import create_engine
+
+load_dotenv()
+
+
+class Environment(str, Enum):
+    DEVELOPMENT = 'development'
+    PRODUCTION = 'production'
+
+
+class Config(BaseSettings):
+    env: Optional[Environment]
+
+    pgsql_host: str
+    pgsql_port: int
+    pgsql_user: str
+
+    pgsql_password: str
+    pgsql_database: str
+
+    access_token_secret: str
+    refresh_token_secret: str
+
+    @property
+    def debug(self):
+        return self.env == Environment.DEVELOPMENT
+
+    @property
+    def pgsql_dsn(self):
+        """postgresql database connection string"""
+
+        return 'postgresql://%s:%s@%s:%s/%s' % (
+            self.pgsql_user,
+            self.pgsql_password,
+            self.pgsql_host,
+            self.pgsql_port,
+            self.pgsql_database
+        )
+
+
+config = Config()
+engine = create_engine(url=config.pgsql_dsn, echo=config.debug)
