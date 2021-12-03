0a8a528b3aba5e126aa423e9aff1966c25481986
jp calvo <calvojp92@gmail.com>
2021-12-03 19:06:45 +0800

    added r prefix

--- api/api/utils/validator.py
+++ api/api/utils/validator.py
@@ -4,7 +4,7 @@ from typing import Any
 
 def validate_url(subject: Any):
     # ref: http://urlregex.com/
-    pattern = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+    pattern = r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
     pattern = re.compile(pattern, re.I | re.M)
 
     return isinstance(subject, str) and pattern.match(subject) is not None
