5b865e5fa98e495bc02f4cae61aa7653cfa90929
jp calvo <calvojp92@gmail.com>
2021-12-04 09:03:47 +0800

    updates

--- api/api/routers/place.py
+++ api/api/routers/place.py
@@ -2,11 +2,14 @@ from datetime import datetime, timezone
 from typing import Optional
 
 from fastapi import APIRouter, status
+from fastapi.datastructures import UploadFile
 from fastapi.exceptions import HTTPException
-from fastapi.param_functions import Depends, Path
+from fastapi.param_functions import Body, Depends, File, Path
+from pydantic import BaseModel, Field
 from sqlmodel import Session, select
 
 from ..config import engine
+from ..dependencies import get_current_user
 from ..models import (CreatePlace, Paginated, Place, ReadPlace, UpdatePlace,
                       User)
 
@@ -33,7 +36,7 @@ class Query:
 )
 async def readall(query: Query = Depends()):
     with Session(engine) as session:
-        stmt = select(User, Place)
+        stmt = select(Place)
         rows = session.exec(stmt).all()
 
         return rows
@@ -59,15 +62,75 @@ async def readone(id_: int = Path(..., alias='id')):
     path='/',
     status_code=status.HTTP_201_CREATED,
     response_model=ReadPlace,
-    response_model_exclude_none=True
+    response_model_exclude_none=True,
 )
-async def create(data: CreatePlace):
+async def create(data: CreatePlace, author: User = Depends(get_current_user)):
     with Session(engine) as session:
         place = Place(
             url=data.url,
             title=data.title,
             description=data.description,
-            keywords=data.keywords,
-            author_id=data.author_id,
+            author_id=author.id,
             created_at=datetime.now(timezone.utc)
         )
+
+        session.add(place)
+        session.commit()
+        session.refresh(place)
+
+        return place
+
+
+def verify_owner(id_: int = Path(..., alias='id'), author: User = Depends(get_current_user)):
+    """verifies current user owns the record"""
+
+    with Session(engine) as session:
+        stmt = select(Place).where(id_ == Place.id)
+        place = session.exec(stmt).one_or_none()
+
+        if place is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+        if place.author.id != author.id:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
+        return place
+
+
+class UploadResponse(BaseModel):
+    image: str = Field(..., description='Stream url')
+
+
+@router.put(
+    path='/{id}/image',
+    response_model=UploadResponse,
+    response_model_exclude_none=True
+)
+async def upload(image: UploadFile = File(...), place: Place = Depends(verify_owner)):
+    with Session(engine) as session:
+        # TODO: upload image to directory
+
+        place.image = image
+        place.updated_at = datetime.now(timezone.utc)
+
+        session.add(place)
+        session.commit()
+        session.refresh(place)
+
+        return place
+
+
+@router.patch(path='/{id}', response_model=ReadPlace, response_model_exclude_none=True)
+async def update(place: Place = Depends(verify_owner), data: UpdatePlace = Body(...)):
+    with Session(engine) as session:
+        for k, v in data.dict(exclude_none=True).items():
+            if hasattr(place, k):
+                setattr(place, k, v)
+
+        place.updated_at = datetime.now(timezone.utc)
+
+        session.add(place)
+        session.commit()
+        session.refresh(place)
+
+        return place
