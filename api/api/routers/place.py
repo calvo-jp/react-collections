a1e6e55bec765980ad512e6ceba822f91b6e0bf1
jp calvo <calvojp92@gmail.com>
2021-12-05 16:28:01 +0800

    refactor

--- api/api/routers/place.py
+++ api/api/routers/place.py
@@ -4,12 +4,11 @@ from typing import Optional
 from fastapi import APIRouter, status
 from fastapi.datastructures import UploadFile
 from fastapi.exceptions import HTTPException
-from fastapi.param_functions import Body, Depends, File, Path
+from fastapi.param_functions import Depends, File, Path
 from pydantic import BaseModel, Field
 from sqlmodel import Session, select
 
-from ..config import engine
-from ..dependencies import get_current_user
+from ..dependencies import get_current_user, get_session
 from ..models import (CreatePlace, Paginated, Place, ReadPlace, UpdatePlace,
                       User)
 
@@ -34,12 +33,11 @@ class Query:
     response_model=Paginated[ReadPlace],
     response_model_exclude_none=True
 )
-async def readall(query: Query = Depends()):
-    with Session(engine) as session:
-        stmt = select(Place)
-        rows = session.exec(stmt).all()
+async def readall(*, query: Query = Depends(), session: Session = Depends(get_session)):
+    stmt = select(Place)
+    rows = session.exec(stmt).all()
 
-        return rows
+    return rows
 
 
 @router.get(
@@ -47,15 +45,18 @@ async def readall(query: Query = Depends()):
     response_model=Paginated[ReadPlace],
     response_model_exclude_none=True
 )
-async def readone(id_: int = Path(..., alias='id')):
-    with Session(engine) as session:
-        stmt = select(User, Place).where(Place.id == id_)
-        place = session.exec(stmt).one_or_none()
+async def readone(
+    *,
+    id_: int = Path(..., alias='id'),
+    session: Session = Depends(get_session)
+):
+    stmt = select(User, Place).where(Place.id == id_)
+    place = session.exec(stmt).one_or_none()
 
-        if place is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    if place is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
-        return place
+    return place
 
 
 @router.post(
@@ -64,37 +65,49 @@ async def readone(id_: int = Path(..., alias='id')):
     response_model=ReadPlace,
     response_model_exclude_none=True,
 )
-async def create(data: CreatePlace, author: User = Depends(get_current_user)):
-    with Session(engine) as session:
-        place = Place(
-            url=data.url,
-            title=data.title,
-            description=data.description,
-            author_id=author.id,
-            created_at=datetime.now(timezone.utc)
-        )
+async def create(
+    *,
+    data: CreatePlace,
+    author: User = Depends(get_current_user),
+    session: Session = Depends(get_session)
+):
+    # just making pylance happy here
+    if author.id is None:
+        raise HTTPException(503)
 
-        session.add(place)
-        session.commit()
-        session.refresh(place)
+    place = Place(
+        url=data.url,
+        title=data.title,
+        description=data.description,
+        author_id=author.id,
+        created_at=datetime.now(timezone.utc)
+    )
 
-        return place
+    session.add(place)
+    session.commit()
+    session.refresh(place)
+
+    return place
 
 
-def verify_owner(id_: int = Path(..., alias='id'), author: User = Depends(get_current_user)):
+def verify_owner(
+    *,
+    id_: int = Path(..., alias='id'),
+    author: User = Depends(get_current_user),
+    session: Session = Depends(get_session)
+):
     """verifies current user owns the record"""
 
-    with Session(engine) as session:
-        stmt = select(Place).where(id_ == Place.id)
-        place = session.exec(stmt).one_or_none()
+    stmt = select(Place).where(id_ == Place.id)
+    place = session.exec(stmt).one_or_none()
 
-        if place is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    if place is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
-        if place.author.id != author.id:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    if place.author.id != author.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
 
-        return place
+    return place
 
 
 class UploadResponse(BaseModel):
@@ -106,31 +119,38 @@ class UploadResponse(BaseModel):
     response_model=UploadResponse,
     response_model_exclude_none=True
 )
-async def upload(image: UploadFile = File(...), place: Place = Depends(verify_owner)):
-    with Session(engine) as session:
-        # TODO: upload image to directory
+async def upload(
+    *,
+    place: Place = Depends(verify_owner),
+    image: UploadFile = File(...),
+    session: Session = Depends(get_session)
+):
+    # TODO: upload image
+    place.image = image.filename
+    place.updated_at = datetime.now(timezone.utc)
 
-        place.image = image
-        place.updated_at = datetime.now(timezone.utc)
+    session.add(place)
+    session.commit()
+    session.refresh(place)
 
-        session.add(place)
-        session.commit()
-        session.refresh(place)
-
-        return place
+    return place
 
 
 @router.patch(path='/{id}', response_model=ReadPlace, response_model_exclude_none=True)
-async def update(place: Place = Depends(verify_owner), data: UpdatePlace = Body(...)):
-    with Session(engine) as session:
-        for k, v in data.dict(exclude_none=True).items():
-            if hasattr(place, k):
-                setattr(place, k, v)
+async def update(
+    *,
+    place: Place = Depends(verify_owner),
+    data: UpdatePlace,
+    session: Session = Depends(get_session)
+):
+    for k, v in data.dict(exclude_none=True).items():
+        if hasattr(place, k):
+            setattr(place, k, v)
 
-        place.updated_at = datetime.now(timezone.utc)
+    place.updated_at = datetime.now(timezone.utc)
 
-        session.add(place)
-        session.commit()
-        session.refresh(place)
+    session.add(place)
+    session.commit()
+    session.refresh(place)
 
-        return place
+    return place
