4ff332101e76973590a51dcc2c2680c92fa5a316
jp calvo <calvojp92@gmail.com>
2021-12-05 17:19:45 +0800

    refactor

--- api/api/routers/stream.py
+++ api/api/routers/stream.py
@@ -10,7 +10,7 @@ router = APIRouter(prefix='/streams', tags=['stream'])
 
 
 @router.get(path='/uploads/{filename}', response_class=FileResponse)
-async def view_file(filename: str):
+async def view_file(*, filename: str):
     fullpath = path.join(config.uploads_dir, filename)
 
     if not path.exists(fullpath):
