9fec534dc47408c44e7d2ecad12d42f0e93bb376
jp calvo <calvojp92@gmail.com>
2021-12-06 09:39:56 +0800

    missing codes added

--- api/api/dependency.py
+++ api/api/dependency.py
@@ -22,7 +22,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/sessions')
 
 async def get_current_user(
     *,
-    token: str,
+    token: str = Depends(oauth2_scheme),
     request: Request,
     session: Session = Depends(get_session)
 ):
