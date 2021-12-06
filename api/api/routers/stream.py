c21b7034425f7ababbccc70c6e9fd108478aad65
jp calvo <calvojp92@gmail.com>
2021-12-06 10:59:40 +0800

    refactor

--- api/api/routers/stream.py
+++ api/api/routers/stream.py
@@ -1,4 +1,4 @@
-from os import path
+import os
 
 from fastapi import APIRouter, status
 from fastapi.exceptions import HTTPException
@@ -11,9 +11,9 @@ router = APIRouter(prefix='/streams', tags=['stream'])
 
 @router.get(path='/uploads/{filename}', response_class=FileResponse)
 async def view_file(*, filename: str):
-    fullpath = path.join(config.uploads_dir, filename)
+    fullpath = os.path.join(config.uploads_dir, filename)
 
-    if not path.exists(fullpath):
+    if not os.path.exists(fullpath):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
     return fullpath
