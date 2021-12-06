9f550a7511bccb2b7443cd7d83693a5445f07521
jp calvo <calvojp92@gmail.com>
2021-12-06 10:49:21 +0800

    refactor

--- api/api/utils/jsonwebtoken.py
+++ api/api/utils/jsonwebtoken.py
@@ -11,35 +11,38 @@ from redis import Redis
 
 from ..config import config
 
-_blacklist = Redis(host=config.redis_host, port=config.redis_port, db=0)
-_secretkey = config.access_token_secret
-_algorithm = ALGORITHMS.HS256
+blacklist = Redis(host=config.redis_host, port=config.redis_port, db=0)
 
 
-def _blacklisted(token_id: str):
-    return _blacklist.get(token_id) is not None
+def blacklisted(token_id: str):
+    return blacklist.get(token_id) is not None
 
 
 def sign(payload: dict[str, Any]):
     claims = payload.copy()
 
-    claims['exp'] = (datetime.now() + timedelta(days=14)).timestamp()
     claims['_id'] = uuid4().hex
+    claims['exp'] = (datetime.now() + timedelta(days=14)).timestamp()
 
-    return jwt.encode(claims, _secretkey, _algorithm)
+    return jwt.encode(claims, config.access_token_secret, ALGORITHMS.HS256)
 
 
 def decode(token: str) -> dict[str, Any]:
-    claims = jwt.decode(token, _secretkey, [_algorithm])
+    claims = jwt.decode(token, config.access_token_secret, [ALGORITHMS.HS256])
 
-    if _blacklisted(claims['_id']):
-        raise JWTError()
+    try:
+        assert not blacklisted(claims['_id'])
+        assert isinstance(blacklist['exp'], float)
 
-    return dict(claims)
+        return dict(claims)
+    except (AssertionError, IndexError) as error:
+        raise JWTError() from error
 
 
 def invalidate(token: str):
+    """blacklists a token until it expires"""
+
     claims = decode(token)
     duration = datetime.fromtimestamp(claims['exp']) - datetime.now()
 
-    _blacklist.setex(claims['_id'], duration, "")
+    blacklist.setex(claims['_id'], duration, "")
