98dd695eeb5e77522bacad4751a7e7b6a5589d69
jp calvo <calvojp92@gmail.com>
2021-12-06 16:20:49 +0800

    added router for streaming including files

---
+++ api/api/router/stream.py
@@ -0,0 +1,19 @@
+import os
+
+from fastapi import APIRouter, status
+from fastapi.exceptions import HTTPException
+from fastapi.responses import FileResponse
+
+from ..config import config
+
+router = APIRouter(prefix='/streams', tags=['stream'])
+
+
+@router.get(path='/uploads/{filename}', response_class=FileResponse)
+async def view_uploaded_file(*, filename: str):
+    fullpath = os.path.join(config.uploads_dir, filename)
+
+    if not os.path.exists(fullpath):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    return fullpath
