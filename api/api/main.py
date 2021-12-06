cfb219c6db0fa5ab79e95c84ac31a4a47b2fc03e
jp calvo <calvojp92@gmail.com>
2021-12-06 09:38:28 +0800

    attached session router

--- api/api/main.py
+++ api/api/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .models import create_tables
-from .router import user
+from .router import session, user
 
 app = FastAPI()
 
@@ -14,6 +14,7 @@ app.add_middleware(
 )
 
 app.include_router(router=user.router)
+app.include_router(router=session.router)
 
 
 @app.on_event(event_type='startup')
