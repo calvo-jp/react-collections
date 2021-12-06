819ce0e50c6ee709340fa8d52098bb08766aac48
jp calvo <calvojp92@gmail.com>
2021-12-06 22:24:57 +0800

    bug fixes

--- api/api/routers/session.py
+++ api/api/routers/session.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, status
 from fastapi.exceptions import HTTPException
 from fastapi.param_functions import Depends
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from jose.exceptions import JWTError
 from pydantic import BaseModel
 from sqlmodel import Session, select
 
@@ -39,7 +40,7 @@ async def login(
     if user is None or not checkpw(password, user.password):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
 
-    access_token = jsonwebtoken.sign(dict(sub=user.id))
+    access_token = jsonwebtoken.sign(dict(user_id=user.id))
 
     return dict(
         access_token=access_token,
@@ -54,4 +55,10 @@ async def login(
     dependencies=[Depends(get_current_user)]
 )
 async def logout(*, token: str):
-    jsonwebtoken.invalidate(token)
+    try:
+        jsonwebtoken.invalidate(token)
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Invalid token.'
+        )
