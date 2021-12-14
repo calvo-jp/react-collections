230e1f0b5948d2054ec5d0bcb11e5bdf25e39053
jp calvo <calvojp92@gmail.com>
2021-12-14 13:43:11 +0800

    font update

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import type { AppProps } from 'next/app';
-import * as React from 'react';
-import 'tailwindcss/tailwind.css';
+import "assets/fonts/source-sans-pro/source-sans-pro.css";
+import type { AppProps } from "next/app";
+import * as React from "react";
+import "tailwindcss/tailwind.css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      sans: ["'Source Sans 3", "sans-serif"],
+      serif: ["'Source Sans Pro", "Times New Roman", "serif"],
     },
   },
   variants: {
