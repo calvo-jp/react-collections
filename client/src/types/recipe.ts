b00a054b46920196aaf335b024dfa25ed817a0bc
jp calvo <calvojp92@gmail.com>
2021-12-22 13:10:08 +0800

    recipe interface added

---
+++ client/src/types/recipe.ts
@@ -0,0 +1,21 @@
+import IUser from "./user";
+
+interface IRecipe {
+  id: number;
+  title: string;
+  description: string;
+  author: IUser;
+  ingredients: string[];
+  instructions: string[];
+  tags: string[];
+  /** image url */
+  image: string;
+  /** video url */
+  video?: string;
+  /** average rate from 0-5 */
+  rating: number;
+  createdAt: string;
+  updatedAt?: string;
+}
+
+export default IRecipe;
