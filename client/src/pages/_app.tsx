18ec33164b9444153c757d754504b398181d6986
jp calvo <calvojp92@gmail.com>
2021-12-22 13:11:09 +0800

    added meta vp

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import "assets/fonts/source-sans-pro/source-sans-pro.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import * as React from "react";
 import "tailwindcss/tailwind.css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 };
 
 export default MyApp;
