5ac1f17066cafdd2da3e7eb218c0bd6e653c9a6c
jp calvo <calvojp92@gmail.com>
2021-12-27 12:13:00 +0800

    fixed nothing to repeat error

--- api/api/utils/validators.py
+++ api/api/utils/validators.py
@@ -2,5 +2,5 @@ import re
 
 
 def is_phone(subject: str):
-    pattern = re.compile(r'^(+?63|0)?9\d{9}$')
+    pattern = re.compile(r'^(\+?63|0)?9\d{9}$')
     return pattern.match(subject) is not None
