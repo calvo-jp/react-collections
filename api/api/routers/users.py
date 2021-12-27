277b80647e80db362c5a9530c55cb64350971651
jp calvo <calvojp92@gmail.com>
2021-12-27 15:08:13 +0800

    incomplete post route

--- api/api/routers/users.py
+++ api/api/routers/users.py
@@ -1,10 +1,11 @@
 from typing import Optional
 
+from bcrypt import gensalt, hashpw
 from fastapi import APIRouter, Depends, HTTPException, Path, Query, status
 from sqlmodel import Session, select
 
 from ..dependencies import SearchParams, get_session
-from ..models import Paginated, ReadUser, User
+from ..models import CreateUser, Paginated, ReadUser, User
 
 router = APIRouter(prefix='/users', tags=['users'])
 
@@ -60,5 +61,44 @@ async def findone(
 
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    return user
+
+
+@router.post(
+    path='/',
+    response_model=ReadUser,
+    response_model_exclude_none=True,
+    status_code=status.HTTP_201_CREATED
+)
+async def create(
+    *,
+    data: CreateUser,
+    session: Session = Depends(get_session)
+):
+    encrypted_password = None
+
+    if data.password is not None:
+        encrypted_password = hashpw(data.password.encode('utf-8'), gensalt())
+
+    user = User(
+        first_name=data.name.first,
+        middle_name=data.name.middle,
+        last_name=data.name.last,
+        suffix=data.name.suffix,
+        date_of_birth=data.date_of_birth,
+        gender=data.gender,
+        marital=data.marital,
+        email=data.email,
+        username=data.username,
+        password=encrypted_password,
+        phone_number=data.phone_number,
+        employment_status=data.employment_status,
+        educational_attainment=data.educational_attainment,
+    )
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
 
     return user
