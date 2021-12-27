4af11aa41e32fc93409b7582068a9378e15fc2d8
jp calvo <calvojp92@gmail.com>
2021-12-27 09:39:16 +0800

    refactor

--- api/api/main.py
+++ api/api/main.py
@@ -7,9 +7,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="*",
-    allow_methods="*",
-    allow_headers="*"
+    allow_origins='*',
+    allow_methods='*',
+    allow_headers='*'
 )
 
 
