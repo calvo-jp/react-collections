3eeff440487c6bd81e6d41a4524d107322830a0f
jp calvo <calvojp92@gmail.com>
2021-12-27 11:43:22 +0800

    added search params deps

--- api/api/routers/users.py
+++ api/api/routers/users.py
@@ -1,19 +1,47 @@
-from fastapi import APIRouter, Depends, HTTPException, Path, status
+from typing import Optional
+
+from fastapi import APIRouter, Depends, HTTPException, Path, Query, status
 from sqlmodel import Session, select
 
 from ..dependencies import get_session
-from ..models import ReadUser, User
+from ..models import Paginated, ReadUser, User
 
 router = APIRouter(prefix='/users', tags=['users'])
 
 
+class SearchParams:
+    def __init__(
+        self,
+        *,
+        page: Optional[int] = Query(default=None, ge=1),
+        page_size: Optional[int] = Query(default=None, ge=25, le=100),
+        search: Optional[str] = None
+    ):
+        self.page = page or 1
+        self.page_size = page_size or 25
+        self.search = search
+
+
 @router.get(
     path='/',
-    response_model=list[ReadUser],
+    response_model=Paginated[ReadUser],
     response_model_exclude_none=True
 )
-async def findall(*, session: Session = Depends(get_session)):
-    return session.exec(select(User)).all()
+async def findall(
+    *,
+    params: SearchParams = Depends(),
+    session: Session = Depends(get_session)
+):
+    rows = session.exec(select(User)).all()
+
+    return Paginated(
+        rows=rows,
+        total_rows=0,
+        page=params.page,
+        page_size=params.page_size,
+        has_next=False,
+        search=params.search
+    )
 
 
 @router.get(
