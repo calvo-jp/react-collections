6f627e1180b85cd7f645f79f55e1da65402c00a5
jp calvo <calvojp92@gmail.com>
2021-12-28 10:09:58 +0800

    avatar should always be available. if user has not set it yet, then there should be default

--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -2,13 +2,16 @@ interface IUser {
   id: number;
   name: string;
   email: string;
+
   /** avatar url */
-  avatar?: string;
+  avatar: string;
   createdAt: string;
   updatedAt?: string;
   totalRecipes: number;
+
   /** number of recipes favorited */
   totalFavorites: number;
+
   /** number of recipes rated */
   totalRatesGiven: number;
 }
