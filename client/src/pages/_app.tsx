d6e43f6f37b57da1954ae1144a86e46c1442719c
jp calvo <calvojp92@gmail.com>
2021-12-29 12:52:52 +0800

    removing mui due to poor editor intellisense

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,34 +1,18 @@
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import CssBaseline from "@mui/material/CssBaseline";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import GlobalProvider from "hooks/store/GlobalProvider";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import * as React from "react";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  const theme = createTheme({
-    typography: {
-      fontFamily: ["'IBM Plex Sans'", "sans-serif"].join(),
-    },
-  });
-
   return (
     <React.Fragment>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
 
-      <CssBaseline />
-
-      <ThemeProvider theme={theme}>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
-          <GlobalProvider>
-            <Component {...pageProps} />
-          </GlobalProvider>
-        </LocalizationProvider>
-      </ThemeProvider>
+      <GlobalProvider>
+        <Component {...pageProps} />
+      </GlobalProvider>
     </React.Fragment>
   );
 };
