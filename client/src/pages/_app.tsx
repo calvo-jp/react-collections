c2b48efb0a92a655edf604b0946bdb257b25b26c
jp calvo <calvojp92@gmail.com>
2021-12-29 11:20:56 +0800

    change of font

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import GlobalProvider from "hooks/store/GlobalProvider";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import * as React from "react";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["'IBM Plex Sans'", "sans-serif"].join(),
+    },
+  });
+
   return (
     <React.Fragment>
       <Head>
@@ -15,11 +22,13 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
 
       <CssBaseline />
 
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <GlobalProvider>
-          <Component {...pageProps} />
-        </GlobalProvider>
-      </LocalizationProvider>
+      <ThemeProvider theme={theme}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <GlobalProvider>
+            <Component {...pageProps} />
+          </GlobalProvider>
+        </LocalizationProvider>
+      </ThemeProvider>
     </React.Fragment>
   );
 };
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -5,9 +5,15 @@ class Document_ extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;700&display=swap"
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
           <link
             rel="stylesheet"
