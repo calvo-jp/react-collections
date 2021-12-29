da684dfcf82a7770b97ae896778cbebbfbcf2b80
jp calvo <calvojp92@gmail.com>
2021-12-29 15:57:54 +0800

    added searchbar icon

---
+++ client/src/widgets/icons/Searchbar.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+type SearchbarIconProps = React.SVGProps<SVGSVGElement>;
+
+const SearchbarIcon: React.FC<SearchbarIconProps> = ({
+  children,
+  ...props
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={16}
+      height={16}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+      />
+    </svg>
+  );
+};
+
+export default SearchbarIcon;
