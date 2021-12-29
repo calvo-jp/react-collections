9f259b3e8ed8a861c7c369d82a200bc22d344dbe
jp calvo <calvojp92@gmail.com>
2021-12-29 18:01:42 +0800

    added chevron right icon

---
+++ client/src/widgets/icons/ChevronRight.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+type BellIconProps = React.SVGProps<SVGSVGElement>;
+
+const BellIcon: React.FC<BellIconProps> = ({ children, ...props }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      width={16}
+      height={16}
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M15 19l-7-7 7-7"
+      />
+    </svg>
+  );
+};
+
+export default BellIcon;
