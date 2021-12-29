fa9377f477fb997650a4c1b842ec3658b63def2f
jp calvo <calvojp92@gmail.com>
2021-12-29 16:31:12 +0800

    separated brand (aka logo_

---
+++ client/src/layouts/Brand.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Brand = () => {
+  return (
+    <Link href="/" passHref>
+      <a className="text-xl">Recipe</a>
+    </Link>
+  );
+};
+
+export default Brand;
