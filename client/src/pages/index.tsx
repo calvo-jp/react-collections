9988c72eabe7e9c6c2df093a0f73c99d18c9f356
jp calvo <calvojp92@gmail.com>
2021-12-29 08:45:16 +0800

    removed testing

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,31 +1,5 @@
-import { CreateOutlined } from "@mui/icons-material";
-import DatePicker from "@mui/lab/DatePicker";
-import { Button } from "@mui/material";
-import TextField from "@mui/material/TextField";
-import { Box } from "@mui/system";
-import * as React from "react";
-
 const Landing = () => {
-  const [value, setValue] = React.useState(null);
-
-  return (
-    <div>
-      <Box p={2}>
-        <Button variant="contained" startIcon={<CreateOutlined />}>
-          sample
-        </Button>
-
-        <DatePicker
-          label="Basic example"
-          value={value}
-          onChange={(newValue) => {
-            setValue(newValue);
-          }}
-          renderInput={(params) => <TextField {...params} />}
-        />
-      </Box>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Landing;
