fb54cdd8d407eaeb996a7c2d469fda58e0023046
jp calvo <calvojp92@gmail.com>
2022-01-02 21:43:03 +0800

    refactor

--- client/src/widgets/Button.tsx
+++ client/src/widgets/Button.tsx
@@ -24,15 +24,15 @@ const Button: React.FC<ButtonProps> = ({
   const disabled = props.disabled;
   const primary = variant === 'primary' && !disabled;
   const secondary = variant === 'secondary' && !disabled;
+  const colored = variant && !disabled;
 
   return (
     <button
       className={clsx(
         'p-2 outline-none rounded-md flex items-center gap-1 justify-center',
-        primary &&
-          'text-white border border-blue-500 bg-blue-500 focus:ring-4 focus:ring-blue-200',
-        secondary &&
-          'text-white border border-red-500 bg-red-500 focus:ring-4 focus:ring-red-200',
+        colored && 'text-white border focus:ring-4',
+        primary && 'border-blue-500 bg-blue-500 focus:ring-blue-200',
+        secondary && 'border-red-500 bg-red-500 focus:ring-red-200',
         disabled && 'bg-gray-100 text-gray-400 cursor-not-allowed',
         fullWidth && 'block w-full',
         className
