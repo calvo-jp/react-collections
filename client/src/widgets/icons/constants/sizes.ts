d046a52310e52d2dac3d18ce312b58dd6d4dad94
jp calvo <calvojp92@gmail.com>
2022-01-02 22:24:54 +0800

    updated

--- client/src/widgets/icons/constants/sizes.ts
+++ client/src/widgets/icons/constants/sizes.ts
@@ -1,8 +1,8 @@
 import Icon from 'widgets/icons/types/icon';
 
-type Size = Record<Required<Icon>['size'], string>;
+type Size = Required<Icon>['size'];
 
-const sizes: Size = {
+const sizes: Record<Size, string> = {
   xs: 'w-4 h-4',
   sm: 'w-5 h-5',
   md: 'w-6 h-6',
