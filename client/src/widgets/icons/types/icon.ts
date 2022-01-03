d75cfdf44d6e9383ac61641548d875d1f1f6fd5b
jp calvo <calvojp92@gmail.com>
2022-01-03 16:33:11 +0800

    removed width and height prop

--- client/src/widgets/icons/types/icon.ts
+++ client/src/widgets/icons/types/icon.ts
@@ -1,11 +1,7 @@
 type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
-type StringOrNumber = string | number;
-
 interface Icon extends React.SVGProps<SVGSVGElement> {
   size?: Size;
-  width?: StringOrNumber;
-  height?: StringOrNumber;
 }
 
 export default Icon;
