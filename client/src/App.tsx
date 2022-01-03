c73eafaa2c6ca1d1b8da9d984112ff9132a443ea
jp calvo <calvojp92@gmail.com>
2022-01-03 23:17:25 +0800

    setup for react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,9 +5,17 @@ import '@fontsource/ibm-plex-sans/400.css';
 import '@fontsource/ibm-plex-sans/500.css';
 import '@fontsource/ibm-plex-sans/600.css';
 import '@fontsource/ibm-plex-sans/700.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './pages/Landing';
 
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
