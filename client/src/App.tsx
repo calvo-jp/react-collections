2134cb6f256446cadb632cb61ce25d9442ff0eae
jp calvo <calvojp92@gmail.com>
2022-01-03 23:13:26 +0800

    client initial setup with cra & tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import '@fontsource/ibm-plex-sans/100.css';
+import '@fontsource/ibm-plex-sans/200.css';
+import '@fontsource/ibm-plex-sans/300.css';
+import '@fontsource/ibm-plex-sans/400.css';
+import '@fontsource/ibm-plex-sans/500.css';
+import '@fontsource/ibm-plex-sans/600.css';
+import '@fontsource/ibm-plex-sans/700.css';
+
+const App = () => {
+  return <div>Hello world</div>;
+};
+
+export default App;
