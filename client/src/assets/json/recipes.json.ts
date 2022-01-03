966637a89335a8d507c930bd0807ebd1e37e0677
jp calvo <calvojp92@gmail.com>
2022-01-03 14:52:00 +0800

    added new item

--- client/src/assets/json/recipes.json.ts
+++ client/src/assets/json/recipes.json.ts
@@ -35,6 +35,33 @@ const items: IRecipe[] = [
     },
     createdAt: '2021-01-30',
   },
+  {
+    id: 2,
+    name: 'Tinolang manok',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nulla nobis inventore eum ratione esse maiores! Sint non itaque doloribus!',
+    ingredients: [
+      '16kg Chicken',
+      '1pc mineral water',
+      '10pcs garlic',
+      '4pk baby oil',
+    ],
+    instructions: [],
+    tags: ['Tinola', 'Chicken'],
+    image: '/images/9.jpg',
+    rating: 4,
+    author: {
+      id: 1,
+      name: 'JP Calvo',
+      email: 'calvojp92@gmail.com',
+      avatar: '',
+      createdAt: '2021-01-15',
+      totalRecipes: 2,
+      totalFavorites: 0,
+      totalReviews: 0,
+    },
+    createdAt: '2021-01-30',
+  },
 ];
 
 export default items;
