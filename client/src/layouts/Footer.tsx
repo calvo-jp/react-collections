64ff15bed4ac05b497399018dfe1c1da4af94e93
jp calvo <calvojp92@gmail.com>
2022-01-03 21:03:56 +0800

    temporary mobile style

--- client/src/layouts/Footer.tsx
+++ client/src/layouts/Footer.tsx
@@ -2,7 +2,7 @@ import HelpLinks from './HelpLinks';
 
 const Footer = () => {
   return (
-    <footer className="p-4 px-8 flex items-center justify-between text-sm">
+    <footer className="p-4 px-8 flex flex-col md:flex-row gap-2 items-center justify-between text-sm">
       <p>&copy; Recipes 2021. All rights reserved</p>
 
       <HelpLinks />
