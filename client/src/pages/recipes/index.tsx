c9470c5f12489720912f19299ef31d003b328ef7
jp calvo <calvojp92@gmail.com>
2022-01-08 12:35:20 +0800

    recipe card added

--- client/src/pages/recipes/index.tsx
+++ client/src/pages/recipes/index.tsx
@@ -1,11 +1,13 @@
 import items from 'assets/json/recipes.json';
 import Header from 'layouts/Header';
-import RecipeCard from 'layouts/RecipeCard';
 import Sidebar from 'layouts/Sidebar';
 import { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
 import * as React from 'react';
 import IRecipe from 'types/recipe';
+import Rating from 'widgets/Rating';
 
 interface Props {
   items: IRecipe[];
@@ -37,7 +39,7 @@ const Recipes: NextPage<Props> = ({ items }) => {
             <section className="p-8 flex-grow">
               <div className="grid gap-6 grid-cols-1 lg:grid-cols-2 grid-flow-row-dense">
                 {items.map((item) => (
-                  <RecipeCard key={item.id} data={item} />
+                  <Recipe key={item.id} data={item} />
                 ))}
               </div>
             </section>
@@ -45,6 +47,52 @@ const Recipes: NextPage<Props> = ({ items }) => {
         </main>
       </div>
     </React.Fragment>
+  );
+};
+
+interface RecipeProps {
+  data: IRecipe;
+}
+
+const Recipe = ({ data }: RecipeProps) => {
+  // prettier-ignore
+  const {
+    id,
+    name,
+    image,
+    rating,
+    description
+  } = data
+
+  return (
+    <Link href={'/recipes/'.concat(id.toString())} passHref>
+      <a
+        key={name}
+        className="block bg-white shadow-md group hover:ring-4 hover:ring-blue-200"
+      >
+        <figure className="relative overflow-hidden h-[250px]">
+          <div className="absolute w-full h-full z-20 top-0 left-0 bg-black bg-opacity-50 hidden group-hover:flex items-center justify-center" />
+
+          <Image
+            src={image}
+            alt=""
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+          />
+        </figure>
+
+        <article className="p-4">
+          <h4>{name}</h4>
+
+          <p className="text-sm text-gray-700 truncate">{description}</p>
+
+          <div className="mt-2">
+            <Rating value={rating} />
+          </div>
+        </article>
+      </a>
+    </Link>
   );
 };
 
