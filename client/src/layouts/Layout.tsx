c8a489df3c3f88c31ecac6395df78eac680275cc
jp calvo <calvojp92@gmail.com>
2022-01-08 13:51:20 +0800

    added bg and option to remove bg

--- client/src/layouts/Layout.tsx
+++ client/src/layouts/Layout.tsx
@@ -1,16 +1,20 @@
+import clsx from 'clsx';
 import * as React from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
-interface LayoutProps {}
+interface LayoutProps {
+  /** remove background color */
+  transparent?: boolean;
+}
 
 /** base layout */
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ transparent, children }) => {
   return (
     <React.Fragment>
       <Header />
 
-      <div className="flex">
+      <div className={clsx('flex', !transparent && 'bg-gray-100')}>
         <Sidebar />
 
         <main className="p-8 grow">{children}</main>
