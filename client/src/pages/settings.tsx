3b6e07e207a768ee2db3975a8a236fa643427db1
jp calvo <calvojp92@gmail.com>
2022-01-08 14:21:07 +0800

    empty settings page

---
+++ client/src/pages/settings.tsx
@@ -0,0 +1,14 @@
+import Layout from 'layouts/Layout';
+import Head from 'next/head';
+
+const Settings = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Settings</title>
+      </Head>
+    </Layout>
+  );
+};
+
+export default Settings;
