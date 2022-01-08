d0bb9337b954893150cb32c515a8a998261b250b
jp calvo <calvojp92@gmail.com>
2022-01-08 18:29:02 +0800

    added samples

---
+++ client/src/assets/json/reviews.json.ts
@@ -0,0 +1,22 @@
+import IReview from 'types/review';
+import recipes from './recipes.json';
+import users from './users.json';
+
+const reviews: IReview[] = [
+  {
+    id: 1,
+    rate: 5,
+    body: 'Awesome!!',
+    author: users[0],
+    recipe: recipes[1],
+    createdAt: '2022-01-01 23:05',
+  },
+  {
+    id: 2,
+    rate: 5,
+    body: 'Good.',
+    author: users[1],
+    recipe: recipes[2],
+    createdAt: '2022-01-01 23:05',
+  },
+];
---
+++ client/src/assets/json/users.json.ts
@@ -0,0 +1,26 @@
+import IUser from 'types/user';
+
+const users: IUser[] = [
+  {
+    id: 1,
+    name: 'JP Calvo',
+    email: 'calvojp92@gmail.com',
+    avatar: '',
+    createdAt: '2020-01-10 13:00',
+    totalFavorites: 26,
+    totalRecipes: 103,
+    totalReviews: 32,
+  },
+  {
+    id: 1,
+    name: 'Mary Williams',
+    email: 'marywilliams@yahoo.com',
+    avatar: '',
+    createdAt: '2021-04-10 13:00',
+    totalFavorites: 89,
+    totalRecipes: 21,
+    totalReviews: 44,
+  },
+];
+
+export default users;
