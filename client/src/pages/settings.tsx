d74b46574ff4f0884f2fa8547e41bc75b3cb0a0d
jp calvo <calvojp92@gmail.com>
2022-01-10 20:45:10 +0800

    settings page update

--- client/src/pages/settings.tsx
+++ client/src/pages/settings.tsx
@@ -1,5 +1,13 @@
+import CheckIcon from '@heroicons/react/solid/CheckCircleIcon';
+import CogIcon from '@heroicons/react/solid/CogIcon';
+import ExclamationIcon from '@heroicons/react/solid/ExclamationCircleIcon';
+import BellIcon from '@heroicons/react/solid/MailIcon';
+import PencilAltIcon from '@heroicons/react/solid/PencilAltIcon';
+import UserIcon from '@heroicons/react/solid/UserIcon';
 import Layout from 'layouts/Layout';
 import Head from 'next/head';
+import * as React from 'react';
+import Button from 'widgets/Button';
 
 const Settings = () => {
   return (
@@ -7,8 +15,172 @@ const Settings = () => {
       <Head>
         <title>Settings</title>
       </Head>
+
+      <div className="bg-white p-4 shadow-md flex flex-col gap-4">
+        <GeneralSettings />
+        <Subscriptions />
+        <DangerZone />
+      </div>
     </Layout>
   );
 };
+
+const DangerZone = () => {
+  return (
+    <div>
+      <Heading label="Danger Zone" icon={ExclamationIcon} />
+
+      <div className="p-4">
+        <Button
+          label="Delete Account"
+          variant="outlined"
+          color="secondary"
+          size="sm"
+        />
+      </div>
+    </div>
+  );
+};
+
+const Subscriptions = () => {
+  return (
+    <div>
+      <Heading label="Subscriptions" icon={BellIcon} />
+
+      <ItemContentWrapper>
+        <LabeledCheckbox label="Push" checked />
+        <LabeledCheckbox label="SMS" />
+        <LabeledCheckbox label="Emails" />
+      </ItemContentWrapper>
+    </div>
+  );
+};
+
+const ItemContentWrapper: React.FC = ({ children }) => {
+  return <div className="p-4 flex flex-col gap-2">{children}</div>;
+};
+
+interface LabeledCheckboxProps {
+  label: string;
+  checked?: boolean;
+  onChange?: (value: boolean) => void;
+}
+
+const LabeledCheckbox = ({
+  label,
+  checked,
+  onChange,
+}: LabeledCheckboxProps) => {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const handleClick = () => inputRef.current?.click();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    onChange?.(e.target.checked);
+
+  return (
+    <div className="flex items-center gap-2">
+      <input
+        ref={inputRef}
+        type="checkbox"
+        checked={checked}
+        onChange={handleChange}
+        className="cursor-pointer"
+      />
+
+      <label className="cursor-pointer" onClick={handleClick}>
+        {label}
+      </label>
+    </div>
+  );
+};
+
+const GeneralSettings = () => {
+  return (
+    <div>
+      <Heading icon={UserIcon} label="General Settings" />
+
+      <ItemContentWrapper>
+        <GeneralSettingsItem label="Name" value="JP Calvo">
+          <Button
+            size="xs"
+            color="primary"
+            label="Change"
+            icon={<PencilAltIcon className="w-3 h-3" />}
+            compact
+          />
+        </GeneralSettingsItem>
+
+        <GeneralSettingsItem
+          label="Email"
+          value={
+            <div className="flex items-center gap-2">
+              <div>calvojp92@gmail.com</div>
+              <CheckIcon className="w-4 h-4 fill-green-600" />
+            </div>
+          }
+        >
+          <Button
+            size="xs"
+            color="primary"
+            label="Change"
+            icon={<PencilAltIcon className="w-3 h-3" />}
+            compact
+          />
+          <Button
+            size="xs"
+            color="primary"
+            label="Verify"
+            icon={<CogIcon className="w-3 h-3" />}
+            compact
+          />
+        </GeneralSettingsItem>
+
+        <GeneralSettingsItem label="Password" value="********">
+          <Button
+            size="xs"
+            color="primary"
+            label="Change"
+            icon={<PencilAltIcon className="w-3 h-3" />}
+            compact
+          />
+        </GeneralSettingsItem>
+      </ItemContentWrapper>
+    </div>
+  );
+};
+
+interface GeneralSettingsItemProps {
+  label: string;
+  value: any;
+}
+
+const GeneralSettingsItem: React.FC<GeneralSettingsItemProps> = ({
+  label,
+  value,
+  children,
+}) => {
+  return (
+    <div>
+      <small className="text-sm text-gray-500">{label}</small>
+      <p>{value}</p>
+      <div className="flex gap-2">{children}</div>
+    </div>
+  );
+};
+
+const Heading = ({ icon: SVGIcon, label }: IconedLabel) => {
+  return (
+    <div className="flex items-center gap-1">
+      <SVGIcon className="w-4 h-4" />
+      <h4 className="text-sm text-gray-600">{label}</h4>
+    </div>
+  );
+};
+
+/** typeof label with icon */
+interface IconedLabel {
+  icon: (props: React.ComponentProps<'svg'>) => JSX.Element;
+  label: string;
+}
 
 export default Settings;
