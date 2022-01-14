ab6f788f3d2c9fa6d1944f1c8c24089f8c45f43c
jp calvo <calvojp92@gmail.com>
2022-01-14 23:22:45 +0800

    will find a better alternative soon. something that offers type hints

--- api/src/plugins/jwt.ts
+++ api/src/plugins/jwt.ts
@@ -1,7 +1,7 @@
 import jwt from 'fastify-jwt';
 import fp from 'fastify-plugin';
 
-export default fp(async (fastify) => {
+export default fp(async (fastify, ops) => {
   fastify.register(jwt, {
     secret: fastify.config.ACCESS_TOKEN_SECRETKEY,
     trusted: async (request, decoded) => {
