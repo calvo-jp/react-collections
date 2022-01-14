6fe128f49f114a6a0360f65474d70ed082ea5bb6
jp calvo <calvojp92@gmail.com>
2022-01-14 14:43:42 +0800

    added sample auth implementation which does not work yet

---
+++ api/src/plugins/auth.ts
@@ -0,0 +1,22 @@
+import type { RouteHandler } from 'fastify';
+import fp from 'fastify-plugin';
+
+// TODO: make this work properly and connect to redis
+export default fp((fastify, ops) => {
+  fastify.decorateRequest('user', undefined);
+
+  fastify.decorate<RouteHandler>('authenticate', async (request, reply) => {
+    try {
+      await request.jwtVerify();
+    } catch (error) {
+      reply.log.error(error);
+      reply.unauthorized();
+    }
+  });
+});
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    authenticate: RouteHandler;
+  }
+}
