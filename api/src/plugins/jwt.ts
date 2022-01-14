fe47c291a484f633bfb9eeae9b397ab0355d290f
jp calvo <calvojp92@gmail.com>
2022-01-14 13:57:41 +0800

    moving auth handler to a separate plugin

--- api/src/plugins/jwt.ts
+++ api/src/plugins/jwt.ts
@@ -1,4 +1,3 @@
-import { FastifyReply, FastifyRequest } from 'fastify';
 import jwt from 'fastify-jwt';
 import fp from 'fastify-plugin';
 
@@ -6,17 +5,4 @@ export default fp(async (fastify) => {
   fastify.register(jwt, {
     secret: fastify.config.ACCESS_TOKEN_SECRETKEY,
   });
-
-  fastify.decorate<Fn>('authenticate', async (req, reply) => {
-    const decoded = req.jwtDecode();
-    console.log(decoded);
-  });
 });
-
-type Fn = (req: FastifyRequest, reply: FastifyReply) => any;
-
-declare module 'fastify' {
-  interface FastifyInstance {
-    authenticate: Fn;
-  }
-}
