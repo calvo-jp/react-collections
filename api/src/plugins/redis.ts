116c4fbee01348bc3592e9ce741a3331d2446b35
jp calvo <calvojp92@gmail.com>
2022-01-14 21:04:40 +0800

    using fastify-redis instead of node-redis due to ts slowing down

--- api/src/plugins/redis.ts
+++ api/src/plugins/redis.ts
@@ -1,21 +1,9 @@
 import fp from 'fastify-plugin';
-import { createClient } from 'redis';
+import redis from 'fastify-redis';
 
 export default fp(async (fastify, ops) => {
-  const redis = createClient({
+  fastify.register(redis, {
     url: fastify.config.REDIS_URL,
-  });
-
-  await redis.connect();
-
-  fastify.decorate('redis', redis);
-  fastify.addHook('onClose', async (server) => {
-    await server.redis.disconnect();
+    closeClient: true,
   });
 });
-
-declare module 'fastify' {
-  interface FastifyInstance {
-    redis: ReturnType<typeof createClient>;
-  }
-}
