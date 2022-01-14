0daf28a596ed800ea19dc1722c6fb017d6e8c43c
jp calvo <calvojp92@gmail.com>
2022-01-14 18:34:48 +0800

    added blacklisted token checker

--- api/src/plugins/jwt.ts
+++ api/src/plugins/jwt.ts
@@ -4,5 +4,10 @@ import fp from 'fastify-plugin';
 export default fp(async (fastify) => {
   fastify.register(jwt, {
     secret: fastify.config.ACCESS_TOKEN_SECRETKEY,
+    trusted: async (request, decoded) => {
+      // check if token is blacklisted
+      const tokenId = decoded.tokenId;
+      return !(await fastify.redis.get(tokenId));
+    },
   });
 });
