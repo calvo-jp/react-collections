b5eda12f920bf9187b8a0f0a9aa7f9754a366691
jp calvo <calvojp92@gmail.com>
2022-01-14 23:21:09 +0800

    added redis custom plugin

--- api/src/plugins/redis.ts
+++ api/src/plugins/redis.ts
@@ -1,9 +1,19 @@
 import fp from 'fastify-plugin';
-import redis from 'fastify-redis';
+import { createClient, RedisClientType, RedisScripts } from 'redis';
 
 export default fp(async (fastify, ops) => {
-  fastify.register(redis, {
-    url: fastify.config.REDIS_URL,
-    closeClient: true,
+  const redis = createClient({ url: fastify.config.REDIS_URL });
+
+  await redis.connect();
+
+  fastify.decorate('redis', redis);
+  fastify.addHook('onClose', async (server) => {
+    await server.redis.disconnect();
   });
 });
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    redis: RedisClientType<any, RedisScripts>;
+  }
+}
