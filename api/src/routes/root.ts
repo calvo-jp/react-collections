705fcbe43ad5d5002e2c43e8b697461d661fe6ad
jp calvo <calvojp92@gmail.com>
2022-01-14 10:02:26 +0800

    added details to root

--- api/src/routes/root.ts
+++ api/src/routes/root.ts
@@ -1,9 +1,18 @@
-import { FastifyPluginAsync } from 'fastify'
+import { FastifyPluginAsync } from 'fastify';
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+  fastify.get('/', async (request, reply) => {
+    reply.code(200).send({
+      title: 'recipes',
+      version: '0.0.1',
+      repository: 'https://github.com/calvo-jp/recipes',
+      author: {
+        name: 'jp calvo',
+        email: 'calvojp92@gmail.com',
+        github: 'https://github.com/calvo-jp',
+      },
+    });
+  });
+};
 
 export default root;
