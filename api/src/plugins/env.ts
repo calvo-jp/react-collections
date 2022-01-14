0a7ad045fdae91975101cad32cee414cf041c463
jp calvo <calvojp92@gmail.com>
2022-01-14 16:22:43 +0800

    added strict

--- api/src/plugins/env.ts
+++ api/src/plugins/env.ts
@@ -2,12 +2,14 @@ import { Static, Type } from '@sinclair/typebox';
 import env from 'fastify-env';
 import fp from 'fastify-plugin';
 
-const TConfig = Type.Object({
-  NODE_ENV: Type.Optional(Type.String()),
-  ACCESS_TOKEN_SECRETKEY: Type.String(),
-  REDIS_HOST: Type.String(),
-  REDIS_PORT: Type.Number(),
-});
+const TConfig = Type.Strict(
+  Type.Object({
+    NODE_ENV: Type.Optional(Type.String()),
+    ACCESS_TOKEN_SECRETKEY: Type.String(),
+    REDIS_HOST: Type.String(),
+    REDIS_PORT: Type.Number(),
+  })
+);
 
 export default fp(async (fastify) => {
   fastify.register(env, {
