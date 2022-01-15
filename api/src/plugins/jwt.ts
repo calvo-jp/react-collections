89e63573ed24747b26d45a0d6e6be6a3a7d861e5
jp calvo <calvojp92@gmail.com>
2022-01-15 09:26:07 +0800

    jwt enhancement

--- api/src/plugins/jwt.ts
+++ api/src/plugins/jwt.ts
@@ -2,12 +2,46 @@ import jwt from 'fastify-jwt';
 import fp from 'fastify-plugin';
 
 export default fp(async (fastify, ops) => {
+  const blacklisted = async (tokenId: string) => {
+    return !!(await fastify.redis.get(tokenId));
+  };
+
   fastify.register(jwt, {
     secret: fastify.config.ACCESS_TOKEN_SECRETKEY,
-    trusted: async (request, decoded) => {
-      // check if token is blacklisted
-      const tokenId = decoded.tokenId;
-      return !(await fastify.redis.get(tokenId));
+    sign: {
+      algorithm: 'HS256',
+      expiresIn: 60 * 60 * 24 * 14, // 14days
+      notBefore: 3, // 3sec
+    },
+    verify: {
+      algorithms: ['HS256'],
+      cache: true,
+      maxAge: 1000, // 1sec
+    },
+    trusted: async (request, payload) => {
+      return !(await blacklisted(payload.tokenId));
+    },
+    messages: {
+      authorizationTokenExpiredMessage: 'Expired token',
+      noAuthorizationInHeaderMessage: 'Invalid token',
+      authorizationTokenUntrusted: 'Invalid token',
+      authorizationTokenInvalid: 'Invalid token',
+      badRequestErrorMessage: 'Missing or invalid token',
     },
   });
 });
+
+declare module 'fastify-jwt' {
+  interface FastifyJWT {
+    payload: {
+      tokenId: string;
+      [key: string]: any;
+    };
+  }
+}
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    authenticate: any;
+  }
+}
