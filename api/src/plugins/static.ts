b4827d3b6d04dc60a957ef738b6be5872bd37504
jp calvo <calvojp92@gmail.com>
2022-01-16 11:55:04 +0800

    reverted changes here

--- api/src/plugins/static.ts
+++ api/src/plugins/static.ts
@@ -1,9 +1,10 @@
 import fp from 'fastify-plugin';
 import static_ from 'fastify-static';
+import * as path from 'node:path';
 
 export default fp(async (fastify, ops) => {
   fastify.register(static_, {
-    root: fastify.config.UPLOADS_DIR,
+    root: path.resolve('src/uploads'),
     prefix: '/images',
     index: false,
     decorateReply: false,
