bdc130ea86fd810c52d1978335a67710ecab80a2
jp calvo <calvojp92@gmail.com>
2022-01-16 21:38:47 +0800

    changes: - automate adding service - typings are also auto, so this file no longer requires changes when new services are added - added successful connection log in dev mode

--- api/src/plugins/database.ts
+++ api/src/plugins/database.ts
@@ -2,37 +2,52 @@ import { PrismaClient } from '@prisma/client';
 import fp from 'fastify-plugin';
 import services from '../services';
 
-interface Collection {
-  user: ReturnType<typeof services.user>;
-  recipe: ReturnType<typeof services.recipe>;
-}
+type Services = typeof services;
+type Collection = {
+  [K in keyof Services]: ReturnType<Services[K]>;
+};
+type DbInstance = PrismaClient;
 
 interface Db {
-  instance: PrismaClient;
+  /** aka `PrismaClient` */
+  instance: DbInstance;
+  /** aka `services` */
   collection: Collection;
 }
 
 export default fp(
   async (fastify, ops) => {
-    const prisma = new PrismaClient();
+    const instance = new PrismaClient();
+    await instance.$connect();
 
-    await prisma.$connect();
+    if (fastify.config.DEBUG) fastify.log.info('Connected to pgsql');
 
     const db: Db = {
-      instance: prisma,
-      collection: {
-        user: services.user(prisma),
-        recipe: services.recipe(prisma),
-      },
+      instance,
+      collection: automate(instance),
     };
 
-    fastify.decorate('db', db);
-    fastify.addHook('onClose', async (server) => {
+    fastify.decorate('db', db).addHook('onClose', async (server) => {
       await server.db.instance.$disconnect();
     });
   },
-  { name: 'database' }
+  {
+    name: 'database',
+    dependencies: ['dotenv'],
+  }
 );
+
+/** automatically adds new services added in the `services/index.ts` file */
+const automate = (db: PrismaClient) => {
+  const collection: Partial<Collection> = {};
+
+  // prettier-ignore
+  for (const [name, init] of Object.entries(services))
+    // @ts-ignore
+    collection[name] = init(db);
+
+  return collection as Collection;
+};
 
 declare module 'fastify' {
   interface FastifyInstance {
