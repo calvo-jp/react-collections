7b207ede8c158d01902e586d6e51dcecca1e783b
jp calvo <calvojp92@gmail.com>
2022-01-16 11:56:28 +0800

    changes reverted

--- api/src/plugins/env.ts
+++ api/src/plugins/env.ts
@@ -1,17 +1,17 @@
 import { Static, Type } from '@sinclair/typebox';
 import env from 'fastify-env';
 import fp from 'fastify-plugin';
-import * as path from 'node:path';
 
 const TConfig = Type.Strict(
   Type.Object({
-    NODE_ENV: Type.Optional(Type.String()),
     ACCESS_TOKEN_SECRETKEY: Type.String(),
     REDIS_URL: Type.String(),
+    NODE_ENV: Type.Optional(Type.String()),
+    TZ: Type.String(),
   })
 );
 
-export default fp(async (fastify) => {
+export default fp(async (fastify, ops) => {
   fastify.register(env, {
     schema: TConfig,
     dotenv: true,
@@ -19,8 +19,6 @@ export default fp(async (fastify) => {
 
   // adding config.DEBUG prop to easily check if NODE_ENV is set to dev
   fastify.addHook('onRegister', async (request) => {
-    request.config.UPLOADS_DIR = path.resolve('src/uploads');
-
     if (request.config.NODE_ENV) {
       const pattern = /^(dev|development|test)$/i;
       request.config.DEBUG = pattern.test(request.config.NODE_ENV);
@@ -32,7 +30,6 @@ declare module 'fastify' {
   interface FastifyInstance {
     config: Static<typeof TConfig> & {
       DEBUG?: boolean;
-      UPLOADS_DIR: string;
     };
   }
 }
