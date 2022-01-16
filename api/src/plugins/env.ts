8d28ceeb48a1e18b81385db41968fc28d54c72c9
jp calvo <calvojp92@gmail.com>
2022-01-16 14:19:24 +0800

    updated env to register custom plugin for calculated configs

--- api/src/plugins/env.ts
+++ api/src/plugins/env.ts
@@ -1,6 +1,7 @@
 import { Static, Type } from '@sinclair/typebox';
 import env from 'fastify-env';
 import fp from 'fastify-plugin';
+import * as path from 'node:path';
 
 // prettier-ignore
 const TConfig = Type.Strict(
@@ -11,22 +12,33 @@ const TConfig = Type.Strict(
         Type.Literal('development')
       ])
     ),
-    DEBUG: Type.Optional(Type.Boolean()),
     ACCESS_TOKEN_SECRETKEY: Type.String(),
     REDIS_URL: Type.String(),
     TZ: Type.String(),
   })
 );
 
-export default fp(async (fastify, ops) => {
-  fastify.register(env, {
-    schema: TConfig,
-    dotenv: true,
-  });
-});
+export default fp(
+  async (fastify) => {
+    fastify.register(env, {
+      schema: TConfig,
+      dotenv: true,
+    });
+
+    // calculated config or config based on .env vars
+    fastify.register(async (server) => {
+      server.config.DEBUG = server.config.NODE_ENV === 'development';
+      server.config.UPLOADS_DIR = path.resolve('src/uploads');
+    });
+  },
+  { name: 'dotenv' }
+);
 
 declare module 'fastify' {
   interface FastifyInstance {
-    config: Static<typeof TConfig>;
+    config: Static<typeof TConfig> & {
+      DEBUG: boolean;
+      UPLOADS_DIR: string;
+    };
   }
 }
