422e13685489ab4f2525120a157a2f7a61b7c810
jp calvo <calvojp92@gmail.com>
2022-01-17 15:11:56 +0800

    refactor

--- api/src/plugins/helmet.ts
+++ api/src/plugins/helmet.ts
@@ -1,9 +1,6 @@
 import helmet from 'fastify-helmet';
 import fp from 'fastify-plugin';
 
-export default fp(
-  async (fastify, ops) => {
-    fastify.register(helmet);
-  },
-  { name: 'helmet' }
-);
+export default fp(async (fastify, ops) => fastify.register(helmet), {
+  name: 'helmet',
+});
