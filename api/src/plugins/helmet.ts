3a8b78c08b70b19eebb8b17487f95902ee2794cb
jp calvo <calvojp92@gmail.com>
2022-01-17 14:30:28 +0800

    added helmet

---
+++ api/src/plugins/helmet.ts
@@ -0,0 +1,9 @@
+import helmet from 'fastify-helmet';
+import fp from 'fastify-plugin';
+
+export default fp(
+  async (fastify, ops) => {
+    fastify.register(helmet);
+  },
+  { name: 'helmet' }
+);
