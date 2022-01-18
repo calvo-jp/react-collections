6f5431b250c72a78a74ef872bae575e777c1d52e
jp calvo <calvojp92@gmail.com>
2022-01-18 19:32:17 +0800

    added response schema and portfolio for my personal website

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,7 +1,29 @@
-import { FastifyPluginAsync } from 'fastify';
+import { Type } from '@sinclair/typebox';
+import type { FastifyPluginAsync, RouteShorthandOptions } from 'fastify';
 
-const index: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async (request, reply) => {
+const ResponseSchema = Type.Object({
+  title: Type.String(),
+  version: Type.String(),
+  repository: Type.String(),
+  author: Type.Object({
+    name: Type.String(),
+    email: Type.String(),
+    github: Type.String(),
+    twitter: Type.String(),
+    portfolio: Type.String(),
+  }),
+});
+
+const index: FastifyPluginAsync = async (fastify): Promise<void> => {
+  const cfg: RouteShorthandOptions = {
+    schema: {
+      response: {
+        200: ResponseSchema,
+      },
+    },
+  };
+
+  fastify.get('/', cfg, async (request, reply) => {
     reply.code(200).send({
       title: 'recipes',
       version: '0.0.1',
@@ -11,6 +33,7 @@ const index: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
         email: 'calvojp92@gmail.com',
         github: 'https://github.com/calvo-jp',
         twitter: 'https://twitter.com/calvo__jp',
+        portfolio: 'https://calvo-jp.vercel.app',
       },
     });
   });
