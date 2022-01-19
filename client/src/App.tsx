412ed3dcc5c29127206a4cdec346c58619bf58e8
jp calvo <calvojp92@gmail.com>
2022-01-19 21:05:32 +0800

    added and tested router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Landing from './pages/Landing';
+import NotFound from './pages/NotFound';
+
 const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
