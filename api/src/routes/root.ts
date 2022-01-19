8e339968baa54bd9350ef3470509d900766df1fd
jp calvo <calvojp92@gmail.com>
2022-01-19 21:20:22 +0800

    added deps and config for index route

--- api/src/routes/root.ts
+++ api/src/routes/root.ts
@@ -1,7 +1,30 @@
-import type { FastifyPluginAsync } from 'fastify';
+import { Type } from '@sinclair/typebox';
+import type { FastifyPluginAsync, RouteShorthandOptions } from 'fastify';
+
+const ResponseOK = Type.Object({
+  name: Type.String(),
+  description: Type.String(),
+  version: Type.String(),
+  repository: Type.String(),
+  author: Type.Object({
+    name: Type.String(),
+    email: Type.String(),
+    github: Type.String(),
+    twitter: Type.String(),
+    facebook: Type.String(),
+  }),
+});
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
+  const config: RouteShorthandOptions = {
+    schema: {
+      response: {
+        200: ResponseOK,
+      },
+    },
+  };
+
+  fastify.get('/', config, async (request, reply) => {
     reply.code(200).send({
       name: 'Pets',
       description: 'Buy or sell a pet',
