3c82918b0ab96dba765e2723aae88e5836be61a3
jp calvo <calvojp92@gmail.com>
2022-01-22 16:15:56 +0800

    /images changed to /uploads since files served includes videos

--- api/src/plugins/static.ts
+++ api/src/plugins/static.ts
@@ -2,14 +2,14 @@ import fp from 'fastify-plugin';
 import static_ from 'fastify-static';
 
 export default fp(
-  async (fastify, ops) => {
+  async (fastify) => {
     fastify.register(static_, {
+      list: fastify.config.DEBUG,
       root: fastify.config.UPLOADS_DIR,
-      prefix: '/images',
+      prefix: '/uploads',
+      dotfiles: 'ignore',
       index: false,
       decorateReply: false,
-      dotfiles: 'ignore',
-      list: fastify.config.DEBUG,
       prefixAvoidTrailingSlash: true,
     });
   },
