c2abe4121767e257b0dff432dd6fcbe12e6dab54
jp calvo <calvojp92@gmail.com>
2022-01-25 16:21:00 +0800

    partial review routes

---
+++ api/src/routes/reviews/index.ts
@@ -0,0 +1,21 @@
+import type { FastifyPluginAsync, RouteShorthandOptions } from 'fastify';
+import TPaginated from '../../shared/typebox/paginated';
+import TReview from '../../shared/typebox/review';
+
+const router: FastifyPluginAsync = async (fastify, ops) => {
+  const service = fastify.db.collection.review;
+
+  const readAllOps: RouteShorthandOptions = {
+    schema: {
+      response: {
+        200: TPaginated(TReview),
+      },
+    },
+  };
+
+  fastify.get('/', readAllOps, async (request, reply) => {
+    return await service.read.all();
+  });
+};
+
+export default router;
