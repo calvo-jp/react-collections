19b1e189326a44f83cb395ab26f836a9bae7cfe8
jp calvo <calvojp92@gmail.com>
2022-01-26 15:16:05 +0800

    added size option

--- client/src/widgets/Rating.tsx
+++ client/src/widgets/Rating.tsx
@@ -2,14 +2,22 @@ import StarIcon from '@heroicons/react/solid/StarIcon';
 import clsx from 'clsx';
 import * as React from 'react';
 
+type Size = 'sm' | 'md' | 'lg' | 'xl' | 'lg';
+
 interface RatingProps {
+  size?: Size;
   value?: number;
   /** onChange won't work if this is set to true */
   readonly?: boolean;
   onChange?: (value: number) => void;
 }
 
-const Rating: React.FC<RatingProps> = ({ value, readonly, onChange }) => {
+const Rating: React.FC<RatingProps> = ({
+  size = 'md',
+  value,
+  readonly,
+  onChange,
+}) => {
   const currentValue = value || 0;
 
   const handleChange = (newValue: number) => {
@@ -27,7 +35,11 @@ const Rating: React.FC<RatingProps> = ({ value, readonly, onChange }) => {
           key={number}
           onClick={handleChange(number)}
           className={clsx(
-            'w-5 h-5 transition-colors duration-300',
+            'transition-colors duration-300',
+            size === 'sm' && 'w-4 h-4',
+            size === 'md' && 'w-5 h-5',
+            size === 'lg' && 'w-7 h-7',
+            size === 'xl' && 'w-9 h-9',
             number > currentValue ? 'fill-gray-300' : 'fill-amber-500',
             !readonly && 'cursor-pointer hover:fill-amber-400'
           )}
