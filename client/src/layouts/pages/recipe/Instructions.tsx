78a935c11e3a1c29a6e9bcc8645a3b6c46d7f3ae
jp calvo <calvojp92@gmail.com>
2022-01-26 18:06:10 +0800

    partial ui updates

--- client/src/layouts/pages/recipe/Instructions.tsx
+++ client/src/layouts/pages/recipe/Instructions.tsx
@@ -11,43 +11,47 @@ interface InstructionsProps {
 
 const Instructions = ({ items }: InstructionsProps) => {
   const [currentItem, setCurrentItem] = React.useState(items.at(0));
-  const [playing, setPlaying] = React.useState(false);
 
   React.useEffect(() => {
     return () => {
       setCurrentItem(undefined);
-      setPlaying(false);
     };
   }, []);
 
+  // TODO: mobile ui
+
   return (
-    <div className="flex gap-2">
-      <div className="w-2/3">
-        <VideoPlayer data={currentItem} />
-      </div>
+    <div>
+      <div className="sm:hidden"></div>
 
-      <div className="w-1/3">
-        <div className="flex flex-col gap-2">
-          <Button
-            variant="outlined"
-            color="primary"
-            label="Add Instruction"
-            icon={<PlusIcon className="w-4 h-4" />}
-            size="sm"
-          />
+      <div className="hidden sm:flex gap-3">
+        <div className="w-2/3">
+          <VideoPlayer data={currentItem} />
+        </div>
 
-          {items.map((item) => {
-            const selected = !!currentItem && currentItem.id === item.id;
+        <div className="w-1/3">
+          <div className="flex flex-col gap-2">
+            <Button
+              variant="outlined"
+              color="primary"
+              label="Add Instruction"
+              icon={<PlusIcon className="w-4 h-4" />}
+              size="sm"
+            />
 
-            return (
-              <Instruction
-                key={item.id}
-                selected={selected}
-                onClick={setCurrentItem}
-                {...item}
-              />
-            );
-          })}
+            {items.map((item) => {
+              const selected = !!currentItem && currentItem.id === item.id;
+
+              return (
+                <Instruction
+                  key={item.id}
+                  selected={selected}
+                  onClick={setCurrentItem}
+                  {...item}
+                />
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
