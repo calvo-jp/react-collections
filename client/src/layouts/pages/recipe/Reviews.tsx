15eaae7665becb15637f645c73cb56c9037c1602
jp calvo <calvojp92@gmail.com>
2022-01-26 19:27:35 +0800

    additional ui

--- client/src/layouts/pages/recipe/Reviews.tsx
+++ client/src/layouts/pages/recipe/Reviews.tsx
@@ -1,3 +1,6 @@
+import HeartIcon from '@heroicons/react/outline/HeartIcon';
+import ChevronLeftIcon from '@heroicons/react/solid/ChevronLeftIcon';
+import ChevronRightIcon from '@heroicons/react/solid/ChevronRightIcon';
 import IReview from 'types/review';
 import Review from './Review';
 
@@ -5,14 +8,35 @@ interface ReviewsProps {
   items: IReview[];
 }
 
-const Reviews = ({ items }: ReviewsProps) => {
+const Reviews = (props: ReviewsProps) => {
   return (
-    <div>
-      <div className="flex flex-col gap-4">
-        {items.map((item) => (
-          <Review {...item} key={item.id} />
-        ))}
+    <div className="flex flex-col gap-4">
+      {props.items.map((item) => (
+        <Review {...item} key={item.id} />
+      ))}
+
+      <div className="flex gap-4">
+        <Pagination />
+
+        <button className="flex items-center gap-1 text-blue-600">
+          <HeartIcon className="w-4 h-4" />
+          <span className="text-sm">Post a feedback</span>
+        </button>
       </div>
+    </div>
+  );
+};
+
+const Pagination = () => {
+  return (
+    <div className="flex items-center text-sm gap-1">
+      <button>
+        <ChevronLeftIcon className="w-4 h-4" />
+      </button>
+      <span>Page 1 of 4</span>
+      <button>
+        <ChevronRightIcon className="w-4 h-4" />
+      </button>
     </div>
   );
 };
