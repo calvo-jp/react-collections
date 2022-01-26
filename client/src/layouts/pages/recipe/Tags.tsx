c4bbcf907f2020fdc83dfbc2ac4dfc16739c5b98
jp calvo <calvojp92@gmail.com>
2022-01-26 15:03:38 +0800

    added add new tag button

--- client/src/layouts/pages/recipe/Tags.tsx
+++ client/src/layouts/pages/recipe/Tags.tsx
@@ -1,15 +1,24 @@
+import PlusIcon from '@heroicons/react/outline/PlusIcon';
+
 interface TagsProps {
   items: string[];
 }
 
 const Tags = (props: TagsProps) => {
   return (
-    <ul className="flex flex-wrap gap-1">
+    <ul className="flex flex-wrap gap-1 items-center">
       {props.items.map((item) => (
         <li key={item} className="text-sm p-2 bg-blue-100">
           {item}
         </li>
       ))}
+
+      <li>
+        <button className="text-sm p-2 flex items-center gap-1 text-blue-500 hover:text-blue-600 ">
+          <PlusIcon className="w-4 h-4" />
+          <span>Create Tag</span>
+        </button>
+      </li>
     </ul>
   );
 };
