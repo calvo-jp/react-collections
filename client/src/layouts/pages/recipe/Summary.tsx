4d5fb0e2d58879c61d04735afee1ab5e54adf51f
jp calvo <calvojp92@gmail.com>
2022-01-26 16:30:26 +0800

    ui updates

--- client/src/layouts/pages/recipe/Summary.tsx
+++ client/src/layouts/pages/recipe/Summary.tsx
@@ -1,19 +1,24 @@
+import HeartIcon from '@heroicons/react/solid/HeartIcon';
 import StarIcon from '@heroicons/react/solid/StarIcon';
 
 const Summary = () => {
   return (
-    <div>
-      <div className="bg-gradient-to-r from-yellow-500 to-orange-400 p-4 py-3 shadow-md w-fit">
-        <div className="text-white w-[125px] flex flex-col items-center">
-          <div className="text-sm">Average Rating</div>
+    <div className="flex gap-3">
+      <div className="bg-gradient-to-r from-orange-400 to-yellow-500 p-4 py-3 shadow-md w-[200px] text-white flex items-center gap-2">
+        <StarIcon className="w-16 h-16" />
 
-          <div className="flex mt-1">
-            <StarIcon className="w-6 h-6" />
-            <StarIcon className="w-6 h-6 opacity-40" />
-            <StarIcon className="w-6 h-6 opacity-40" />
-            <StarIcon className="w-6 h-6 opacity-40" />
-            <StarIcon className="w-6 h-6 opacity-40" />
-          </div>
+        <div>
+          <div className="text-sm">Average Rate</div>
+          <div className="text-2xl">4</div>
+        </div>
+      </div>
+
+      <div className="bg-gradient-to-r from-red-400 to-pink-400 p-4 py-3 shadow-md w-[200px] text-white flex items-center gap-2">
+        <HeartIcon className="w-16 h-16" />
+
+        <div>
+          <div className="text-sm">Total hearts</div>
+          <div className="text-2xl">1</div>
         </div>
       </div>
     </div>
