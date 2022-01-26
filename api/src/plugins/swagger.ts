755080563e3795239aaad3b07ba1790df32f320a
jp calvo <calvojp92@gmail.com>
2022-01-26 08:13:43 +0800

    swagger plugin init

---
+++ api/src/plugins/swagger.ts
@@ -0,0 +1,33 @@
+import fp from 'fastify-plugin';
+import swagger from 'fastify-swagger';
+
+// TODO: live docs should have a different config
+export default fp(
+  async (fastify, ops) => {
+    fastify.register(swagger, {
+      mode: 'dynamic',
+      staticCSP: true,
+      routePrefix: '/docs',
+      exposeRoute: true,
+      swagger: {
+        info: {
+          title: 'Recipes API Documentation',
+          description: 'Official API documentation for recipes',
+          version: '0.0.1',
+          contact: {
+            name: 'JP Calvo',
+            email: 'calvojp92@gmail.com',
+          },
+        },
+        host: 'localhost',
+        schemes: ['http'],
+        consumes: ['application/json'],
+        produces: ['application/json'],
+      },
+    });
+  },
+  {
+    name: 'swagger',
+    dependencies: ['dotenv'],
+  }
+);
