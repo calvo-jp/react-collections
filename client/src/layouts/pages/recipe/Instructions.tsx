febefa159a092cab4433a7b60bcc6af3043a9a37
jp calvo <calvojp92@gmail.com>
2022-01-26 16:51:52 +0800

    added button for adding new record

--- client/src/layouts/pages/recipe/Instructions.tsx
+++ client/src/layouts/pages/recipe/Instructions.tsx
@@ -1,7 +1,7 @@
-import ChevronLeftIcon from '@heroicons/react/solid/ChevronLeftIcon';
-import ChevronRightIcon from '@heroicons/react/solid/ChevronRightIcon';
+import PlusIcon from '@heroicons/react/outline/PlusIcon';
 import * as React from 'react';
 import IInstruction from 'types/instruction';
+import Button from 'widgets/Button';
 import Instruction from './Instruction';
 import VideoPlayer from './VideoPlayer';
 
@@ -28,6 +28,14 @@ const Instructions = ({ items }: InstructionsProps) => {
 
       <div className="w-1/3">
         <div className="flex flex-col gap-2">
+          <Button
+            variant="outlined"
+            color="primary"
+            label="Add Instruction"
+            icon={<PlusIcon className="w-4 h-4" />}
+            size="sm"
+          />
+
           {items.map((item) => {
             const selected = !!currentItem && currentItem.id === item.id;
 
@@ -41,18 +49,6 @@ const Instructions = ({ items }: InstructionsProps) => {
             );
           })}
         </div>
-
-        {items.length > 5 && (
-          <div className="mt-3 flex items-center justify-end gap-2">
-            <button>
-              <ChevronLeftIcon className="w-5 h-5" />
-            </button>
-            <span className="text-sm">Page 3 of 4</span>
-            <button>
-              <ChevronRightIcon className="w-5 h-5" />
-            </button>
-          </div>
-        )}
       </div>
     </div>
   );
