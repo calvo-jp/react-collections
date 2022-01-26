f3f3057fa95e0d9ee38c123e00bd8cd51529d853
jp calvo <calvojp92@gmail.com>
2022-01-26 17:09:51 +0800

    added item menu toggler

--- client/src/layouts/pages/recipe/Instruction.tsx
+++ client/src/layouts/pages/recipe/Instruction.tsx
@@ -1,3 +1,4 @@
+import { DotsHorizontalIcon } from '@heroicons/react/outline';
 import clsx from 'clsx';
 import IInstruction from 'types/instruction';
 
@@ -19,13 +20,22 @@ const Instruction = ({ onClick, selected, ...data }: InstructionProps) => {
         'cursor-pointer border p-3 flex gap-4 items-center',
         !selected &&
           'border-gray-200 hover:border-gray-300 transition-colors duration-300',
-        selected && 'border-green-300'
+        selected && 'border-green-400'
       )}
     >
-      <div className="truncate">
+      <div className="truncate flex-grow">
         <div className="text-sm truncate">{data.description}</div>
         <div className="text-[13px] text-gray-400">3 mins ago</div>
       </div>
+
+      <button
+        className="border border-transparent hover:border-gray-200 rounded-full p-0.5"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <DotsHorizontalIcon className="w-5 h-5" />
+      </button>
     </a>
   );
 };
