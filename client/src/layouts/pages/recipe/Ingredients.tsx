829b13ede025a31ed2d47437f626b08893494b79
jp calvo <calvojp92@gmail.com>
2022-01-26 15:08:39 +0800

    added add new button

--- client/src/layouts/pages/recipe/Ingredients.tsx
+++ client/src/layouts/pages/recipe/Ingredients.tsx
@@ -1,14 +1,23 @@
+import PlusIcon from '@heroicons/react/outline/PlusIcon';
+
 interface IngredientsProps {
   items: string[];
 }
 
 const Ingredients = (props: IngredientsProps) => {
   return (
-    <ul className="list-disc pl-4">
-      {props.items.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
-    </ul>
+    <div>
+      <ul className="list-disc pl-4">
+        {props.items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+
+      <button className="text-sm flex items-center gap-1 text-blue-600 mt-2">
+        <PlusIcon className="w-4 h-4" />
+        <span>Add Ingredient</span>
+      </button>
+    </div>
   );
 };
 
