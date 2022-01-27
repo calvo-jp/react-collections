1f055a1a4e8daa22a4f8e2ec360b6b483e5f65d1
jp calvo <calvojp92@gmail.com>
2022-01-27 18:15:35 +0800

    updated globalStore

--- client/src/hooks/store/action.ts
+++ client/src/hooks/store/action.ts
@@ -1,3 +1,18 @@
-type StoreAction = {};
+import IUser from 'types/user';
+
+interface NavbarAction {
+  type: 'navbar.toggle';
+}
+
+interface LoginAction {
+  type: 'session.login';
+  payload: IUser;
+}
+
+interface LogoutAction {
+  type: 'session.logout';
+}
+
+type StoreAction = NavbarAction | LoginAction | LogoutAction;
 
 export default StoreAction;
--- client/src/hooks/store/state.ts
+++ client/src/hooks/store/state.ts
@@ -1,3 +1,9 @@
-interface StoreState {}
+import IUser from 'types/user';
+
+interface StoreState {
+  authorized?: boolean;
+  currentUser?: IUser;
+  navbarOpened?: boolean;
+}
 
 export default StoreState;
