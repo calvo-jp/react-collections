a75fbf5d6e2cd2d4493d2e8fe74cc7129d92bb27
jp calvo <calvojp92@gmail.com>
2022-01-28 12:23:02 +0800

    using headerTwo

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,5 +1,6 @@
 import Footer from 'layouts/Footer';
 import Header from 'layouts/Header';
+import HeaderTwo from 'layouts/HeaderTwo';
 import Head from 'next/head';
 import * as React from 'react';
 
@@ -11,7 +12,7 @@ const NotFound = () => {
       </Head>
 
       <div className="flex flex-col min-h-screen">
-        <Header />
+        <HeaderTwo url="/" label="Go to homepage" />
 
         <main className="flex-grow flex justify-center items-center">
           <div className="text-9xl font-bold text-gray-300">4O4</div>
