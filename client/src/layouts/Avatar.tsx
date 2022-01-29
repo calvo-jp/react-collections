b9d17bea00f82b978dc246c88c405bad73e60b4b
jp calvo <calvojp92@gmail.com>
2022-01-29 19:58:14 +0800

    fix accent color should only be blue

--- client/src/layouts/Avatar.tsx
+++ client/src/layouts/Avatar.tsx
@@ -20,7 +20,7 @@ const Avatar = (props: AvatarProps) => {
         />
       </div>
 
-      <button className="flex items-center justify-center z-10 absolute right-1 bottom-1 bg-gradient-to-r from-cyan-500 h-12 w-12 to-blue-400 rounded-full border-4 border-gray-100 group">
+      <button className="flex items-center justify-center z-10 absolute right-1 bottom-1 bg-gradient-to-r from-blue-500 h-12 w-12 to-blue-400 rounded-full border-4 border-gray-100 group">
         <CameraIcon className="fill-white w-7 h-7 group-hover:w-8 group-hover:h-8 transition-all duration-100" />
       </button>
     </div>
