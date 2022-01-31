42f0ebe060323030711dcd1c2438a8ebdff4a28f
jp calvo <calvojp92@gmail.com>
2022-01-31 17:14:03 +0800

    responsive size

--- client/src/layouts/pages/recipe/Jumbotron.tsx
+++ client/src/layouts/pages/recipe/Jumbotron.tsx
@@ -6,7 +6,7 @@ interface JumbotronProps {
 
 const Jumbotron = (props: JumbotronProps) => {
   return (
-    <figure className="relative h-[300px]">
+    <figure className="relative h-[250px] md:h-[300px] lg:h-[350px] xl:h-[400px]">
       <Image
         src={props.src}
         alt=""
