5558ef4add501ff100e61a578ceb3eb43bebfa59
jp calvo <calvojp92@gmail.com>
2022-01-31 16:49:30 +0800

    updated 404 page

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,5 +1,4 @@
 import Footer from 'layouts/Footer';
-import Header from 'layouts/Header';
 import HeaderTwo from 'layouts/HeaderTwo';
 import Head from 'next/head';
 import * as React from 'react';
@@ -15,7 +14,9 @@ const NotFound = () => {
         <HeaderTwo redirect="/" label="Go to homepage" />
 
         <main className="flex-grow flex justify-center items-center">
-          <div className="text-9xl font-bold text-gray-300">4O4</div>
+          <div>
+            <h1 className="text-9xl font-extrabold text-gray-200">404</h1>
+          </div>
         </main>
 
         <Footer />
