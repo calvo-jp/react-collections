a18bfdb995ae4aa65c5d030561764eb07f82afdd
jp calvo <calvojp92@gmail.com>
2022-02-02 17:55:53 +0800

    init bookmarks page

---
+++ client/src/pages/bookmarks.tsx
@@ -0,0 +1,14 @@
+import Layout from 'layouts/Layout';
+import Head from 'next/head';
+
+const Bookmarks = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Favorites</title>
+      </Head>
+    </Layout>
+  );
+};
+
+export default Bookmarks;
