5354335cccef4d0fbcf50603a65619d1130952f0
jp calvo <calvojp92@gmail.com>
2022-02-02 17:57:01 +0800

    favorites changed to bookmark

--- client/src/types/bookmark.ts
+++ client/src/types/bookmark.ts
@@ -1,11 +1,11 @@
 import IRecipe from './recipe';
 import IUser from './user';
 
-interface Favorite {
+interface IBookmark {
   id: number;
   author: IUser;
   recipe: IRecipe | null;
   createdAt: string;
 }
 
-export default Favorite;
+export default IBookmark;
