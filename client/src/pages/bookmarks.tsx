065d8387f8ebe618a2b59d92feb062ae7ed183ba
jp calvo <calvojp92@gmail.com>
2022-02-03 18:01:17 +0800

    partial bookmarks page ui

--- client/src/pages/bookmarks.tsx
+++ client/src/pages/bookmarks.tsx
@@ -1,5 +1,12 @@
+import XIcon from '@heroicons/react/outline/XIcon';
+import recipes from 'assets/samples/json/recipes.json';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import Layout from 'layouts/Layout';
 import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import IRecipe from 'types/recipe';
 
 const Bookmarks = () => {
   return (
@@ -7,7 +14,55 @@ const Bookmarks = () => {
       <Head>
         <title>Favorites</title>
       </Head>
+
+      <div>
+        <ul className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-2">
+          {recipes.map((recipe) => (
+            <li key={recipe.id}>
+              <Bookmark {...recipe} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </Layout>
+  );
+};
+
+const Bookmark = ({ id, name, description, banner, createdAt }: IRecipe) => {
+  const router = useRouter();
+  const path = router.asPath;
+
+  const date = formatDistanceToNow(new Date(createdAt), {
+    addSuffix: true,
+    includeSeconds: true,
+  });
+
+  return (
+    <Link href={`/recipes/${id}?redirect=${encodeURIComponent(path)}`} passHref>
+      <a className="p-4 flex items-center gap-4 shadow-md bg-white">
+        <div className="relative h-[85px] basis-[85px] shrink-0 grow-0 rounded-full overflow-hidden">
+          <Image
+            src={banner!}
+            alt=""
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+          />
+        </div>
+
+        <div className="grow min-w-0">
+          <div className="md:text-lg">{name}</div>
+          <p className="text-sm text-gray-600 whitespace-nowrap overflow-hidden text-ellipsis">
+            {description}
+          </p>
+          <div className="text-xs text-gray-500">{date}</div>
+        </div>
+
+        <button>
+          <XIcon className="w-6 h-6 text-gray-200" />
+        </button>
+      </a>
+    </Link>
   );
 };
 
