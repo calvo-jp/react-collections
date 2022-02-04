9bdf0f9bb329b09b5dac92b6275b3e99a1b05b06
jp calvo <calvojp92@gmail.com>
2022-02-04 16:44:52 +0800

    card moved to a different component for reuse

---
+++ client/src/layouts/cards/Recipe.tsx
@@ -0,0 +1,60 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import IRecipe from 'types/recipe';
+import Rating from 'widgets/Rating';
+
+interface RecipeCardProps {
+  data: IRecipe;
+  width?: number;
+  height?: number;
+}
+
+const RecipeCard = (props: RecipeCardProps) => {
+  const router = useRouter();
+
+  const {
+    data: { id, name, description, banner, summary },
+    width,
+    height,
+  } = props;
+
+  return (
+    <Link
+      href={`/recipes/${id}?redirect=${encodeURIComponent(router.pathname)}`}
+      passHref
+    >
+      <a className="block bg-white shadow-md hover:ring-4 hover:ring-blue-200">
+        <figure
+          className="relative"
+          style={{
+            width,
+            height,
+          }}
+        >
+          <Image
+            src={banner!}
+            alt=""
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+          />
+        </figure>
+
+        <article className="p-4">
+          <h4 className="text-lg">{name}</h4>
+
+          <p className="text-sm text-gray-700 whitespace-nowrap overflow-hidden text-ellipsis">
+            {description}
+          </p>
+
+          <div className="mt-2">
+            <Rating value={summary.rating} />
+          </div>
+        </article>
+      </a>
+    </Link>
+  );
+};
+
+export default RecipeCard;
--- client/src/pages/recipes/index.tsx
+++ client/src/pages/recipes/index.tsx
@@ -1,12 +1,10 @@
 import recipes from 'assets/samples/json/recipes.json';
+import RecipeCard from 'layouts/cards/Recipe';
 import Layout from 'layouts/Layout';
 import type { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
 import * as React from 'react';
 import IRecipe from 'types/recipe';
-import Rating from 'widgets/Rating';
 
 interface Props {
   items: IRecipe[];
@@ -30,58 +28,10 @@ const Recipes: NextPage<Props> = ({ items }) => {
 
       <div className="grid gap-4 lg:gap-6 grid-cols-1 sm:grid-cols-2 grid-flow-row-dense">
         {items.map((item) => (
-          <Recipe key={item.id} data={item} />
+          <RecipeCard key={item.id} data={item} height={250} />
         ))}
       </div>
     </Layout>
-  );
-};
-
-interface RecipeProps {
-  data: IRecipe;
-}
-
-const Recipe = ({ data }: RecipeProps) => {
-  // prettier-ignore
-  const {
-    id,
-    name,
-    banner,
-    summary,
-    description
-  } = data
-
-  return (
-    <Link href={'/recipes/'.concat(id.toString())} passHref>
-      <a
-        key={name}
-        className="block bg-white shadow-md hover:ring-4 hover:ring-blue-200 group relative"
-      >
-        <figure className="relative overflow-hidden h-[250px]">
-          <div className="absolute w-full h-full z-20 top-0 left-0 bg-black bg-opacity-50 hidden group-hover:flex items-center justify-center" />
-
-          <Image
-            src={banner!}
-            alt=""
-            layout="fill"
-            objectFit="cover"
-            objectPosition="center"
-          />
-        </figure>
-
-        <article className="p-4">
-          <h4 className="text-lg">{name}</h4>
-
-          <p className="text-sm text-gray-700 whitespace-nowrap overflow-hidden text-ellipsis">
-            {description}
-          </p>
-
-          <div className="mt-2">
-            <Rating value={summary.rating} />
-          </div>
-        </article>
-      </a>
-    </Link>
   );
 };
 
