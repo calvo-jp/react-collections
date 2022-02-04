8bb5413b670c4816d73dbffac93708a1c9474858
jp calvo <calvojp92@gmail.com>
2022-02-04 11:06:39 +0800

    added getters

--- client/src/hooks/useQuery.ts
+++ client/src/hooks/useQuery.ts
@@ -1,22 +1,24 @@
 import { useRouter } from 'next/router';
 
-/**
- * @description abstraction of `router.query`
- *
- * @example
- * ```javascript
- * useQuery("page") // { page: undefined | string | string[] }
- * useQuery("page", "pageSize")
- * ...
- * ```
- */
 const useQuery = <T extends string>(...keys: T[]) => {
   const router = useRouter();
   const params: { [P in T]?: string | string[] } = {};
 
   for (const key of keys) params[key] = router.query[key];
 
-  return params;
+  return {
+    ...params,
+
+    __get__(key: keyof typeof params) {
+      return [params[key]].flat().at(0);
+    },
+
+    __getAll__(key: keyof typeof params) {
+      const v = params[key];
+
+      return !!v ? (Array.isArray(v) ? v : [v]) : [];
+    },
+  };
 };
 
 export default useQuery;
