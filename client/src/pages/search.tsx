c7bea07b8cb33ab6d7a246c8c3d2241f3d4acd97
jp calvo <calvojp92@gmail.com>
2022-02-04 11:17:11 +0800

    reverted to init page

--- client/src/pages/search.tsx
+++ client/src/pages/search.tsx
@@ -1,36 +1,11 @@
-import useQuery from 'hooks/useQuery';
 import Head from 'next/head';
 import * as React from 'react';
-import IRecipe from 'types/recipe';
 
 const Search = () => {
-  const query = useQuery('keyword', 'origin');
-
-  const origin = [query.origin].flat(1).at(0) || '/';
-  const keyword = [query.keyword].flat(1).at(0);
-
-  const [loading, setLoading] = React.useState(true);
-  const [records, setRecords] = React.useState<IRecipe[]>([]);
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
-
-  React.useEffect(() => {
-    if (keyword) {
-      // search logic
-    }
-
-    return () => {
-      setLoading(true);
-      setRecords([]);
-    };
-  }, [keyword]);
-
   return (
     <React.Fragment>
       <Head>
-        <title>{!keyword ? 'Search' : keyword}</title>
+        <title>Search</title>
       </Head>
 
       <div></div>
