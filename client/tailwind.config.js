07f70ce9b11872fd31e0f35b31fbe28b51c6ca7e
jp calvo <calvojp92@gmail.com>
2022-02-05 11:47:03 +0800

    refactor

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,47 +1,43 @@
+const fontFamily = {
+  sans: [
+    "'IBM Plex Sans'",
+    '-apple-system',
+    'BlinkMacSystemFont',
+    "'Segoe UI'",
+    'Roboto',
+    'Oxygen',
+    'Ubuntu',
+    'Cantarell',
+    "'Open Sans'",
+    "'Helvetica Neue'",
+    'sans-serif',
+  ],
+};
+
+const fontWeight = {
+  extralight: 100,
+  light: 200,
+  semilight: 300,
+  normal: 400,
+  semibold: 500,
+  bold: 600,
+  extrabold: 700,
+};
+
+/** header height */
 const header = 50;
+
+/** navbar width */
 const navbar = 265;
 
-module.exports = {
+const tailwindConfig = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    fontFamily: {
-      sans: [
-        "'IBM Plex Sans'",
-        '-apple-system',
-        'BlinkMacSystemFont',
-        "'Segoe UI'",
-        'Roboto',
-        'Oxygen',
-        'Ubuntu',
-        'Cantarell',
-        "'Open Sans'",
-        "'Helvetica Neue'",
-        'sans-serif',
-      ],
-    },
-    fontWeight: {
-      extralight: 100,
-      light: 200,
-      semilight: 300,
-      normal: 400,
-      semibold: 500,
-      bold: 600,
-      extrabold: 700,
-    },
-    extend: {
-      height: {
-        header,
-      },
-      width: {
-        navbar,
-      },
-      flexBasis: {
-        navbar,
-      },
-    },
-  },
-  variants: {
-    extend: {},
+    fontFamily,
+    fontWeight,
+    extend: { height: { header }, width: { navbar }, flexBasis: { navbar } },
   },
   plugins: [],
 };
+
+module.exports = tailwindConfig;
