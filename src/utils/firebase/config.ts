9ba7da559e222492c3ce8064a5e186a2cccc10c8
jp calvo <calvojp92@gmail.com>
2022-02-13 21:18:01 +0800

    firebase setup

---
+++ src/utils/firebase/config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBDKnxTGx_1BI6ZQ5QKwh6cdt-lpEmFO8g',
+  authDomain: 'recipes-d076e.firebaseapp.com',
+  projectId: 'recipes-d076e',
+  storageBucket: 'recipes-d076e.appspot.com',
+  messagingSenderId: '1033295662065',
+  appId: '1:1033295662065:web:52e2f1a0f2f8937d805250',
+};
+
+export default firebaseConfig;
