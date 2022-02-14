b652aac05a2f701e40653b563ba53d631156c80d
jp calvo <calvojp92@gmail.com>
2022-02-14 20:52:07 +0800

    ui updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,28 @@
 import ArrowRightIcon from '@heroicons/react/solid/ArrowRightIcon';
-import logo from 'assets/logo.png';
 import wallpaper from 'assets/wallpaper.jpg';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import * as React from 'react';
+import LogoIcon from 'widgets/icons/logo';
 
 const Landing = () => {
   return (
     <React.Fragment>
       <Head>
-        <title>Recipes - Find or search amazing recipes</title>
+        <title>Amazing Recipes - Find or search amazing recipes</title>
       </Head>
 
-      <div className="min-h-screen relative text-white dark:text-zinc-300 flex flex-col p-8 px-12">
+      <div className="min-h-screen relative flex flex-col">
         <BackgroundGradient />
         <BackgroundImage />
 
-        <header className="flex items-center justify-between z-10">
-          <div>
-            <Image src={logo} alt="" width={48} height={48} />
-          </div>
+        <header className="flex justify-between z-10 p-8">
+          <Link href="/" passHref>
+            <a>
+              <LogoIcon />
+            </a>
+          </Link>
 
           <div>
             <Link href="/login" passHref>
@@ -29,15 +31,29 @@ const Landing = () => {
           </div>
         </header>
 
-        <main className="grow flex items-center z-10">
+        <main className="grow flex items-center z-10 p-8">
           <section className="max-w-xl">
             <Article />
           </section>
         </main>
+
+        <footer className="z-10 relative py-4 px-8 flex justify-between items-center">
+          <FooterBackgroundGradient />
+
+          <div className="text-sm">&copy; Amazing Recipes {currentYear}</div>
+        </footer>
       </div>
     </React.Fragment>
   );
 };
+
+const FooterBackgroundGradient = () => {
+  return (
+    <div className="bg-gradient-to-r from-black to-sky-500 absolute w-full h-full left-0 top-0 opacity-25 -z-10" />
+  );
+};
+
+const currentYear = new Date().getFullYear();
 
 const BackgroundImage = () => {
   return (
@@ -63,9 +79,11 @@ const BackgroundGradient = () => {
 const Article = () => {
   return (
     <section className="">
-      <h1 className="text-5xl font-extrabold">Find or share recipes</h1>
+      <h1 className="text-4xl md:text-5xl font-extrabold">
+        Find or share recipes
+      </h1>
 
-      <p className="mt-2 text-lg font-semilight">
+      <p className="mt-2 md:text-lg font-semilight">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam,
         eveniet placeat rem dolor amet iure?
       </p>
