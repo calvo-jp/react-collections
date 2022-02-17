854c88d730614b75fc84dd1753c98d7be52dbc97
jp calvo <calvojp92@gmail.com>
2022-02-17 15:58:18 +0800

    samples included

---
+++ src/assets/samples/json/reviews.json.ts
@@ -0,0 +1,26 @@
+import IReview from 'types/review';
+import recipes from './recipes.json';
+import users from './users.json';
+
+const reviews: IReview[] = [
+  {
+    id: 1,
+    rate: 5,
+    body: 'Awesome!!',
+    author: users[0],
+    recipe: recipes[1],
+    createdAt: '2022-01-01 23:05',
+    updatedAt: '2022-01-01 23:05',
+  },
+  {
+    id: 2,
+    rate: 3,
+    body: 'Good.',
+    author: users[1],
+    recipe: recipes[2],
+    createdAt: '2021-12-24 22:01',
+    updatedAt: '2022-01-01 23:05',
+  },
+];
+
+export default reviews;
---
+++ src/assets/samples/json/users.json.ts
@@ -0,0 +1,36 @@
+import IUser from 'types/user';
+
+const users: IUser[] = [
+  {
+    id: 1,
+    name: 'JP Calvo',
+    email: 'calvojp92@gmail.com',
+    avatar: '',
+    summary: {
+      favorites: 26,
+      recipes: 103,
+      reviews: 32,
+    },
+    emailVerified: false,
+    emailVerifiedAt: null,
+    createdAt: '2020-01-10 13:00',
+    updatedAt: '2020-01-10 13:00',
+  },
+  {
+    id: 2,
+    name: 'Mary Williams',
+    email: 'marywilliams@yahoo.com',
+    avatar: '',
+    summary: {
+      favorites: 89,
+      recipes: 21,
+      reviews: 44,
+    },
+    emailVerified: false,
+    emailVerifiedAt: null,
+    createdAt: '2021-04-10 13:00',
+    updatedAt: '2021-04-10 13:00',
+  },
+];
+
+export default users;
