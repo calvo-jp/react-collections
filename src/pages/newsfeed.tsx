2f764eb8c28b1323cdbfde45ac30df3cbc007df1
jp calvo <calvojp92@gmail.com>
2022-02-18 21:11:13 +0800

    partial ui updates

--- src/pages/newsfeed.tsx
+++ src/pages/newsfeed.tsx
@@ -1,3 +1,6 @@
+import SparklesIcon from '@heroicons/react/solid/SparklesIcon';
+import StarIcon from '@heroicons/react/solid/StarIcon';
+import ThumbUpIcon from '@heroicons/react/solid/ThumbUpIcon';
 import recipes from 'assets/samples/json/recipes.json';
 import RecipeCard from 'layouts/cards/Recipe';
 import Header from 'layouts/Header';
@@ -5,22 +8,92 @@ import Head from 'next/head';
 
 const NewsFeed = () => {
   return (
-    <div className="bg-gray-100 dark:bg-zinc-900 min-h-screen">
+    <>
       <Head>
         <title>Newsfeed</title>
       </Head>
 
-      <Header />
+      <div className="bg-gray-100 dark:bg-zinc-900 min-h-screen">
+        <Header />
+        <Main />
+      </div>
+    </>
+  );
+};
 
-      <main className="p-8 grid grid-cols-3">
-        <div className="col-span-2 grid grid-cols-2 gap-4">
-          {recipes.map((recipe) => (
-            <RecipeCard key={recipe.id} data={recipe} height={200} />
-          ))}
+const Main = () => {
+  return (
+    <main className="p-8">
+      <div className="grid grid-cols-12 gap-8">
+        <div className="col-span-9">
+          <Featured />
+
+          <div className="mt-4">
+            <Popular />
+          </div>
         </div>
-        <div></div>
-      </main>
+
+        <div>
+          <New />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+const New = () => {
+  return (
+    <>
+      <HeadLine label="New" icon={SparklesIcon} />
+
+      <div></div>
+    </>
+  );
+};
+
+const Popular = () => {
+  return (
+    <>
+      <HeadLine label="Popular" icon={ThumbUpIcon} />
+
+      <div className="grid grid-cols-2 gap-4 mt-2">
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.id} data={recipe} height={200} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+const Featured = () => {
+  return (
+    <div>
+      <HeadLine label="Featured" icon={StarIcon} />
+
+      <div className="h-[200px]"></div>
     </div>
+  );
+};
+
+interface HeadLineProps {
+  label: string;
+  icon: (props: React.ComponentProps<'svg'>) => JSX.Element;
+}
+
+const HeadLine = ({ label, icon: SVGIcon }: HeadLineProps) => {
+  return (
+    <div className="flex gap-1 items-center">
+      <SVGIcon className="basis-6 w-6 h-6" />
+      <Heading>{label}</Heading>
+    </div>
+  );
+};
+
+const Heading: React.FC = ({ children }) => {
+  return (
+    <h2 className="uppercase font-heading font-heading-thick text-lg">
+      {children}
+    </h2>
   );
 };
 
