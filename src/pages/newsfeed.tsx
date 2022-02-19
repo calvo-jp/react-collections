b0c7d09232e907f970bd72ae62127f9491472fd5
jp calvo <calvojp92@gmail.com>
2022-02-19 17:48:22 +0800

    ubuntu ui updates

--- src/pages/newsfeed.tsx
+++ src/pages/newsfeed.tsx
@@ -1,3 +1,5 @@
+import ChevronLeftIcon from '@heroicons/react/outline/ChevronLeftIcon';
+import ChevronRightIcon from '@heroicons/react/outline/ChevronRightIcon';
 import SparklesIcon from '@heroicons/react/solid/SparklesIcon';
 import StarIcon from '@heroicons/react/solid/StarIcon';
 import ThumbUpIcon from '@heroicons/react/solid/ThumbUpIcon';
@@ -5,6 +7,7 @@ import recipes from 'assets/samples/json/recipes.json';
 import RecipeCard from 'layouts/cards/Recipe';
 import Header from 'layouts/Header';
 import Head from 'next/head';
+import Image from 'next/image';
 import * as React from 'react';
 
 const NewsFeed = () => {
@@ -24,18 +27,25 @@ const NewsFeed = () => {
 
 const Main = () => {
   return (
-    <main className="p-8">
-      <div className="grid grid-cols-12 gap-8">
-        <div className="col-span-9">
+    <main>
+      <div className="grid grid-cols-12 px-8">
+        <div className="col-span-9 py-8">
           <Featured />
 
-          <div className="mt-4">
+          <div className="mt-8">
             <Popular />
           </div>
         </div>
 
-        <div>
+        <div className="sticky top-header col-span-3 self-start py-8 px-8">
           <New />
+
+          <p className="text-sm mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            laboriosam at molestiae, obcaecati vero alias veniam magnam nemo
+            atque quaerat saepe commodi reprehenderit velit nesciunt facere?
+            Corrupti aperiam tempora amet!
+          </p>
         </div>
       </div>
     </main>
@@ -58,8 +68,15 @@ const Popular = () => {
       <HeadLine label="Popular" icon={ThumbUpIcon} />
 
       <div className="grid grid-cols-2 gap-4 mt-2">
-        {recipes.map((recipe) => (
-          <RecipeCard key={recipe.id} data={recipe} height={200} />
+        {[
+          ...recipes,
+          ...recipes,
+          ...recipes,
+          ...recipes,
+          ...recipes,
+          ...recipes,
+        ].map((recipe, idx) => (
+          <RecipeCard key={idx} data={recipe} height={200} />
         ))}
       </div>
     </React.Fragment>
@@ -71,7 +88,39 @@ const Featured = () => {
     <div>
       <HeadLine label="Featured" icon={StarIcon} />
 
-      <div className="h-[200px]"></div>
+      <div className="relative w-full">
+        <button className="absolute z-10 -left-5 top-[50%] translate-y-[-50%] rounded-full bg-black opacity-50 p-2">
+          <ChevronLeftIcon className="w-8 h-8 stroke-white" />
+        </button>
+
+        <button className="absolute z-10 -right-5 top-[50%] translate-y-[-50%] rounded-full bg-black opacity-50 p-2">
+          <ChevronRightIcon className="w-8 h-8 stroke-white" />
+        </button>
+
+        <div className="flex gap-2 mt-4 w-full overflow-x-auto">
+          {[
+            ...recipes,
+            ...recipes,
+            ...recipes,
+            ...recipes,
+            ...recipes,
+            ...recipes,
+          ].map((recipe) => (
+            <div
+              key={recipe.id}
+              className="relative basis-[200px] shrink-0 grow-0 w-[200px] h-[200px]"
+            >
+              <Image
+                src={recipe.banner!}
+                alt=""
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
