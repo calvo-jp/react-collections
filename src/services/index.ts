7e014d5ae34bf37ac88c0fda47de76226c2e3da1
jp calvo <calvojp92@gmail.com>
2022-02-22 15:17:03 +0800

    added service for todo

---
+++ src/services/index.ts
@@ -0,0 +1,8 @@
+import todo from './todo';
+
+const service = {
+  todo,
+  // place other services here
+};
+
+export default service;
---
+++ src/services/todo.ts
@@ -0,0 +1,63 @@
+import { v4 as uuid } from 'uuid';
+import ITodo from '../types/todo';
+
+const read = {
+  async all(): Promise<ITodo[]> {
+    const response = await fetch('http://localhost:3001/todos');
+    return await response.json();
+  },
+  async one(id: string): Promise<ITodo | null> {
+    try {
+      const response = await fetch('http://localhost:3001/todos/' + id);
+      return await response.json();
+    } catch (exception) {
+      if (import.meta.env.DEV) console.error(exception);
+
+      return null;
+    }
+  },
+};
+
+const create = async (name: string): Promise<ITodo> => {
+  const id = uuid();
+  const createdAt = Date.now();
+
+  const response = await fetch('http://localhost:3001/todos', {
+    method: 'POST',
+    body: JSON.stringify({
+      id,
+      name,
+      createdAt,
+    }),
+  });
+
+  const todo = await response.json();
+  return todo;
+};
+
+type UpdateInput = Partial<Pick<ITodo, 'name' | 'complete'>>;
+
+const update = async (id: string, data: UpdateInput) => {
+  const response = await fetch('http://localhost:3001/todos/' + id, {
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
+
+  const todo = await response.json();
+  return todo;
+};
+
+const remove = async (id: string) => {
+  await fetch('http://localhost:3001/todos/' + id, {
+    method: 'DELETE',
+  });
+};
+
+const todoService = {
+  read,
+  create,
+  update,
+  delete: remove,
+};
+
+export default todoService;
