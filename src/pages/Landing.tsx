826c809f34153c024a5b42a376f4548af68439c1
jp calvo <calvojp92@gmail.com>
2022-02-23 19:58:15 +0800

    floating action button added

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -7,7 +7,7 @@ const Landing = () => {
       <Header>
         <Brand>
           <h1>Money-tor</h1>
-          <p>Monitor your expenses and income.</p>
+          <p>Monitor all your expenses and income.</p>
         </Brand>
       </Header>
 
@@ -32,9 +32,39 @@ const Landing = () => {
           />
         </Items>
       </Main>
+
+      <CreateButton>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path d="M19 16.166c0-4.289-4.465-5.483-7.887-7.091-2.079-1.079-1.816-3.658 1.162-3.832 1.652-.1 3.351.39 4.886.929l.724-3.295c-1.814-.551-3.437-.803-4.885-.841v-2.036h-2v2.134c-3.89.535-5.968 2.975-5.968 5.7 0 4.876 5.693 5.62 7.556 6.487 2.54 1.136 2.07 3.5-.229 4.021-1.993.451-4.538-.337-6.45-1.079l-.909 3.288c1.787.923 3.931 1.417 6 1.453v1.996h2v-2.105c3.313-.464 6.005-2.293 6-5.729z" />
+        </svg>
+      </CreateButton>
     </Container>
   );
 };
+
+const CreateButton = styled.button`
+  position: fixed;
+  bottom: 24px;
+  right: 24px;
+  background: linear-gradient(to right, #ea580c, #d97706);
+  border: none;
+  border-radius: 100%;
+  padding: 0.5rem;
+  cursor: pointer;
+  box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+
+  svg {
+    width: 32px;
+    height: 32px;
+    display: block;
+    fill: white;
+  }
+`;
 
 const Items = styled.section`
   display: flex;
