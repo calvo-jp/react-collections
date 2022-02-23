4df72433724569950bbd365be69d123ab5fe8d7f
jp calvo <calvojp92@gmail.com>
2022-02-23 08:09:27 +0800

    custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'Lato'", 'sans-serif'],
+      },
+      fontWeight: {
+        100: 100,
+        300: 300,
+        400: 400,
+        700: 700,
+        900: 900,
+      },
+    },
   },
   plugins: [],
 };
