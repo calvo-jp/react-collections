63c775d5dcdf80e51b17aa0d84aa7e668b5b0a59
jp calvo <calvojp92@gmail.com>
2022-02-24 10:24:59 +0800

    refactor and partial updates in ui

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
+import CloseIcon from '../components/icons/Close';
+import DollarIcon from '../components/icons/Dollar';
 import Item from '../components/Item';
+import IItem from '../types/item';
+import noop from '../utils/noop';
 
 const Landing = () => {
   return (
@@ -21,6 +25,7 @@ const Landing = () => {
               createdAt: Date.now(),
             }}
           />
+
           <Item
             data={{
               id: 'example-id-2',
@@ -34,18 +39,87 @@ const Landing = () => {
       </Main>
 
       <CreateButton>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path d="M19 16.166c0-4.289-4.465-5.483-7.887-7.091-2.079-1.079-1.816-3.658 1.162-3.832 1.652-.1 3.351.39 4.886.929l.724-3.295c-1.814-.551-3.437-.803-4.885-.841v-2.036h-2v2.134c-3.89.535-5.968 2.975-5.968 5.7 0 4.876 5.693 5.62 7.556 6.487 2.54 1.136 2.07 3.5-.229 4.021-1.993.451-4.538-.337-6.45-1.079l-.909 3.288c1.787.923 3.931 1.417 6 1.453v1.996h2v-2.105c3.313-.464 6.005-2.293 6-5.729z" />
-        </svg>
+        <DollarIcon />
       </CreateButton>
+
+      <CreateItemPopup />
     </Container>
   );
 };
+
+interface CreateItemPopupProps {
+  onCancel?: () => void;
+  onCreate?: (data: IItem) => void;
+}
+
+const CreateItemPopup = ({
+  onCancel,
+  onCreate = noop,
+}: CreateItemPopupProps) => {
+  return (
+    <Modal>
+      <ModalContent>
+        <CloseButton onClick={onCancel}>
+          <CloseIcon />
+        </CloseButton>
+
+        <form></form>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+const TextField = styled.input``;
+
+const CloseButton = styled.button`
+  background-color: transparent;
+  border: none;
+  position: absolute;
+  right: 16px;
+  top: 16px;
+  cursor: pointer;
+
+  svg {
+    display: block;
+    width: 24px;
+    height: 24px;
+    stroke: #e2e8f0;
+    transition: stroke 300ms ease-in-out;
+  }
+
+  &:hover,
+  &:focus {
+    svg {
+      stroke: #cbd5e1;
+    }
+  }
+`;
+
+const ModalContent = styled.div`
+  position: relative;
+  background-color: #fff;
+  border-radius: 0.15rem;
+  width: 100%;
+  height: 100%;
+
+  @media screen and (min-width: 900px) {
+    width: 80%;
+    height: 80%;
+  }
+`;
+
+const Modal = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0%;
+  top: 0;
+  z-index: 99;
+  background-color: #000000a7;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const CreateButton = styled.button`
   position: fixed;
@@ -57,6 +131,7 @@ const CreateButton = styled.button`
   padding: 0.5rem;
   cursor: pointer;
   box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+  z-index: 9;
 
   svg {
     width: 32px;
