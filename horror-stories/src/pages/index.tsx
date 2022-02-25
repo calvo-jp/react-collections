63e919e51ebc2371b4a8366f8a36104338f80092
jp calvo <calvojp92@gmail.com>
2022-02-25 08:57:08 +0800

    tailwind setup

---
+++ horror-stories/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Landing = () => {
+  return (
+    <>
+      <Head>
+        <title>Horror Stories</title>
+      </Head>
+
+      <div>Hello world</div>
+    </>
+  );
+};
+
+export default Landing;
