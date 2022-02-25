740f62730222166fe65867f1fc21a7f5c3ca1c73
jp calvo <calvojp92@gmail.com>
2022-02-25 20:32:11 +0800

    custom font

--- weather-app/tailwind.config.js
+++ weather-app/tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["'Montserrat'", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
