a63945565850270e20415a305122c2cbf2f1033c
jp calvo <calvojp92@gmail.com>
2022-02-25 20:32:15 +0800

    added deps

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,50 +1,5 @@
-import { useEffect, useState } from "react";
-import getWeather from "./utils/getWeather";
-
 const App = () => {
-  const [weather, setWeather] = useState<Record<string, any> | null>(null);
-  const [pending, setPending] = useState(true);
-
-  useEffect(() => {
-    getWeather()
-      .then(setWeather)
-      .finally(() => setPending(false));
-
-    return () => {
-      setWeather(null);
-      setPending(true);
-    };
-  }, []);
-
-  if (pending) return <div>Loading...</div>;
-  if (!weather) return <div>Something went wrong.</div>;
-
-  return (
-    <div>
-      <main>
-        <section>
-          <img src={weather.current.condition.icon} alt="" />
-
-          <div>{weather.current.condition.text}</div>
-          <div>{weather.current.feelslike_c}</div>
-        </section>
-        <section>
-          <div>{weather.location.name}</div>
-          <div>{weather.location.country}</div>
-        </section>
-      </main>
-
-      <footer>
-        <div>
-          {"Powered by "}
-
-          <a href="https://www.weatherapi.com/" title="Free Weather API">
-            WeatherAPI.com
-          </a>
-        </div>
-      </footer>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
