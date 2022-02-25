0a025896ebfbb7917a303c624cf2922bdde0016c
jp calvo <calvojp92@gmail.com>
2022-02-25 21:03:26 +0800

    weather-app sucks. gotta find a different app

--- weather-app/src/utils/getWeather.ts
+++
@@ -1,9 +0,0 @@
-const getWeather = async () => {
-  const response = await fetch(
-    "http://api.weatherapi.com/v1/current.json?key=61c0cad003b84cecad191330222402&q=auto:ip"
-  );
-
-  return await response.json();
-};
-
-export default getWeather;
