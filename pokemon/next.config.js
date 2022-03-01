ae9c3eee7cb6566d7e579b1f3ad2810da4d93c6e
jp calvo <calvojp92@gmail.com>
2022-03-01 12:20:15 +0800

    added permanent redirect

--- pokemon/next.config.js
+++ pokemon/next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/pokemons",
+        permanent: true,
+        statusCode: 301,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
