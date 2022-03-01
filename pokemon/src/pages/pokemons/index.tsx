8829301a8295c9b44a36016f0640545c32b33f59
jp calvo <calvojp92@gmail.com>
2022-03-01 14:13:18 +0800

    updates (still needs pagination)

--- pokemon/src/pages/pokemons/index.tsx
+++ pokemon/src/pages/pokemons/index.tsx
@@ -3,27 +3,44 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import IPokemon from "types/pokemon";
+import normalizePokemonObject from "utils/normalizePokemonObject";
 
 interface Props {
   pokemons: IPokemon[];
 }
 
+interface IResult {
+  name: string;
+  url: string;
+}
+interface IPaginated {
+  count: number;
+  results: IResult[];
+  next: string | null;
+  previous: string | null;
+}
+
 export const getStaticProps: GetStaticProps<Props> = async () => {
+  const endpoint = "https://pokeapi.co/api/v2/pokemon";
+  const response = await fetch(endpoint);
+  const data: IPaginated = await response.json();
+
+  const promises = data.results.map(({ url }) => fetch(url));
+  const results = await Promise.allSettled(promises);
+  const pokemons: IPokemon[] = [];
+
+  for (const result of results) {
+    if (result.status === "fulfilled") {
+      const parsed = await result.value.json();
+      const pokemon = normalizePokemonObject(parsed);
+
+      pokemons.push(pokemon);
+    }
+  }
+
   return {
+    props: { pokemons },
     revalidate: 60,
-    props: {
-      pokemons: [
-        {
-          id: 1,
-          image: "/unknown.png",
-          name: "pikachu",
-          abilities: [],
-          moves: [],
-          stats: [],
-          types: [],
-        },
-      ],
-    },
   };
 };
 
