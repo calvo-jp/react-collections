5e4302a82f0a697db405014cc5b7e8546497a9a1
jp calvo <calvojp92@gmail.com>
2022-03-01 12:40:00 +0800

    added normalizer for api responses

---
+++ pokemon/src/utils/normalizePokemonObject.ts
@@ -0,0 +1,18 @@
+import IPokemon from "../types/pokemon";
+
+type Dict = Record<string, any>;
+
+export default function normalizePokemonObject(data: Dict): IPokemon {
+  return {
+    id: data.id,
+    name: data.name,
+    image: data.sprites.other.dream_world.front_default,
+    types: data.types.map((type: Dict) => type.type.name),
+    abilities: data.abilities.map((ability: Dict) => ability.ability.name),
+    moves: data.moves.map((move: Dict) => move.move.name).slice(0, 10),
+    stats: data.stats.map((stat: Dict) => ({
+      value: stat.base_stat,
+      name: stat.stat.name,
+    })),
+  };
+}
